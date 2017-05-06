f0425365f9cc9706d5180c7bd798cbb32adb8fac
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2017-05-06 15:39:21 +0300

    Fix build on Windows with Qt4 as well

--- src/synchronization/AuthenticationManager_p.h
+++ src/synchronization/AuthenticationManager_p.h
@@ -21,13 +21,13 @@
 
 #include <quentier/synchronization/AuthenticationManager.h>
 
-#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
-
 #ifdef _MSC_VER
 // God, MSVC is such a piece of CRAP! See https://msdn.microsoft.com/en-us/library/h5sh3k99.aspx
 #define true 1
 #define false 0
 #endif
+
+#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
 
 #include <qt5qevercloud/VersionInfo.h>
 
@@ -47,6 +47,11 @@
 #include <qt4qevercloud/VersionInfo.h>
 #if !QEVERCLOUD_HAS_OAUTH
 #error "The used QEverCloud library has no OAuth support"
+#endif
+
+#ifdef _MSC_VER
+#undef true
+#undef false
 #endif
 
 #include <qt4qevercloud/QEverCloudOAuth.h>
