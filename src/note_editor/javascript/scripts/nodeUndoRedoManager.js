2cafe859769a5aed41c000bb12bb13b065dd6e43
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2017-08-15 07:37:59 +0300

    Issue #2: add preservation and restoration of selection into NodeUndoRedoManager although it would not work in all circumstances

--- src/note_editor/javascript/scripts/nodeUndoRedoManager.js
+++ src/note_editor/javascript/scripts/nodeUndoRedoManager.js
@@ -19,49 +19,67 @@
 function NodeUndoRedoManager() {
     this.undoNodes = [];
     this.undoNodeInnerHtmls = [];
+    this.undoSavedSelections = [];
 
     this.redoNodes = [];
     this.redoNodeInnerHtmls = [];
+    this.redoSavedSelections = [];
 
     this.lastError;
 
-    this.undoRedoImpl = function(sourceNodes, sourceNodeInnerHtmls, destNodes, destNodeInnerHtmls, performingUndo) {
+    this.undoRedoImpl = function(sourceNodes, sourceNodeInnerHtmls, sourceSavedSelections,
+                                 destNodes, destNodeInnerHtmls, destSavedSelections, performingUndo) {
         var actionString = (performingUndo ? "undo" : "redo");
 
         if (!sourceNodes) {
-            lastError = "can't " + actionString + " the resource action: no source nodes helper array";
+            lastError = "can't " + actionString + ": no source nodes helper array";
             console.warn(lastError);
             return { status:false, error:lastError };
         }
 
         if (!sourceNodeInnerHtmls) {
-            lastError = "can't " + actionString + " the resource action: no source node inner html helper array";
+            lastError = "can't " + actionString + ": no source node inner html helper array";
+            console.warn(lastError);
+            return { status:false, error:lastError };
+        }
+
+        if (!sourceSavedSelections) {
+            lastError = "can't " + actionString + ": no source saved selections array";
             console.warn(lastError);
             return { status:false, error:lastError };
         }
 
         var sourceNode = sourceNodes.pop();
         if (!sourceNode) {
-            lastError = "can't " + actionString + " the resource action: no source node";
+            lastError = "can't " + actionString + ": no source node";
             console.warn(lastError);
             return { status:false, error:lastError };
         }
 
         var sourceNodeInnerHtml = sourceNodeInnerHtmls.pop();
         if (!sourceNodeInnerHtml) {
-            lastError = "can't " + actionString + " the resource action: no source node's inner html";
+            lastError = "can't " + actionString + ": no source node's inner html";
+            console.warn(lastError);
+            return { status:false, error:lastError };
+        }
+
+        var sourceSavedSelection = sourceSavedSelections.pop();
+        if (!sourceSavedSelection) {
+            lastError = "can't " + actionString + ": no source saved selection";
             console.warn(lastError);
             return { status:false, error:lastError };
         }
 
         destNodes.push(sourceNode);
         destNodeInnerHtmls.push(sourceNode.innerHTML);
+        destSavedSelections.push(selectionManager.saveSelection());
 
         console.log("Html before: " + sourceNode.innerHTML + "; html to paste: " + sourceNodeInnerHtml);
 
         observer.stop();
         try {
             sourceNode.innerHTML = sourceNodeInnerHtml;
+            selectionManager.restoreSelection(sourceSavedSelection);
         }
         finally {
             observer.start();
@@ -73,40 +91,48 @@ function NodeUndoRedoManager() {
 
 NodeUndoRedoManager.prototype.undo = function() {
     console.log("NodeUndoRedoManager: undo");
-    return this.undoRedoImpl(this.undoNodes, this.undoNodeInnerHtmls,
-                             this.redoNodes, this.redoNodeInnerHtmls, true);
+    return this.undoRedoImpl(this.undoNodes, this.undoNodeInnerHtmls, this.undoSavedSelections,
+                             this.redoNodes, this.redoNodeInnerHtmls, this.redoSavedSelections, true);
 }
 
 NodeUndoRedoManager.prototype.redo = function() {
     console.log("NodeUndoRedoManager: redo");
-    return this.undoRedoImpl(this.redoNodes, this.redoNodeInnerHtmls,
-                             this.undoNodes, this.undoNodeInnerHtmls, false);
+    return this.undoRedoImpl(this.redoNodes, this.redoNodeInnerHtmls, this.redoSavedSelections,
+                             this.undoNodes, this.undoNodeInnerHtmls, this.undoSavedSelections, false);
 }
 
 NodeUndoRedoManager.prototype.pushUndo = function(node, html) {
+    console.log("NodeUndoRedoManager: pushUndo");
     this.undoNodes.push(node);
     this.undoNodeInnerHtmls.push(html);
+    this.undoSavedSelections.push(selectionManager.saveSelection());
 }
 
 NodeUndoRedoManager.prototype.pushRedo = function(node, html) {
+    console.log("NodeUndoRedoManager: pushRedo");
     this.redoNodes.push(node);
     this.redoNodeInnerHtmls.push(html);
+    this.redoSavedSelections.push(selectionManager.saveSelection());
 }
 
 NodeUndoRedoManager.prototype.popUndo = function() {
     var node = undoNodes.pop();
     var html = undoNodeInnerHtmls.pop();
+    var selection = undoSavedSelections.pop();
     var res = [];
     res.push(node);
     res.push(html);
+    res.push(selection);
     return res;
 }
 
 NodeUndoRedoManager.prototype.popRedo = function() {
     var node = redoNodes.pop();
     var html = redoNodeInnerHtmls.pop();
+    var selection = redoSavedSelections.pop();
     res.push(node);
     res.push(html);
+    res.push(selection);
     return res;
 }
 
