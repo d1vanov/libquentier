f56a2617fa9e84f4e3ee2517b20684e5ccc23a6f
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2018-08-05 20:34:04 +0300

    d1vanov/quentier#192: suppress clazy warning

--- src/synchronization/SynchronizationManager_p.cpp
+++ src/synchronization/SynchronizationManager_p.cpp
@@ -1471,7 +1471,7 @@ void SynchronizationManagerPrivate::authenticateToLinkedNotebooks()
         }
 
         // 2) Set up the job writing the shard id to the keychain
-        jobIt = m_writeLinkedNotebookShardIdJobIdsWithLinkedNotebookGuids.left.find(guid);
+        jobIt = m_writeLinkedNotebookShardIdJobIdsWithLinkedNotebookGuids.left.find(guid);  // clazy:exclude=rule-of-two-soft
         if (jobIt == m_writeLinkedNotebookShardIdJobIdsWithLinkedNotebookGuids.left.end()) {
             QString key = keyPrefix + LINKED_NOTEBOOK_SHARD_ID_KEY_PART + guid;
             QUuid jobId = m_pKeychainService->startWritePasswordJob(WRITE_LINKED_NOTEBOOK_SHARD_ID_JOB, key, shardId);
