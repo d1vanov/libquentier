9022ccbd37c483ab247a0dcae4d08e1cf681ceca
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2020-05-03 22:15:06 +0300

    Issue #135: implement QEverCloud logger

--- src/logging/QuentierLogger.cpp
+++ src/logging/QuentierLogger.cpp
@@ -18,28 +18,130 @@
 
 #include <quentier/logging/QuentierLogger.h>
 #include "QuentierLogger_p.h"
+
+#include <qt5qevercloud/Log.h>
+
 #include <QCoreApplication>
 
+#include <exception>
+
 namespace quentier {
+
+////////////////////////////////////////////////////////////////////////////////
+
+LogLevel::type QEverCloudLogLevelToQuentierLogLevel(
+    const qevercloud::LogLevel logLevel)
+{
+    switch(logLevel)
+    {
+    case qevercloud::LogLevel::Trace:
+        return LogLevel::TraceLevel;
+    case qevercloud::LogLevel::Debug:
+        return LogLevel::DebugLevel;
+    case qevercloud::LogLevel::Info:
+        return LogLevel::InfoLevel;
+    case qevercloud::LogLevel::Warn:
+        return LogLevel::WarnLevel;
+    case qevercloud::LogLevel::Error:
+        return LogLevel::ErrorLevel;
+    default:
+        return LogLevel::InfoLevel;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+class QEverCloudLogger final: public qevercloud::ILogger
+{
+public:
+    virtual bool shouldLog(
+        const qevercloud::LogLevel level, const char * component) const override
+    {
+        Q_UNUSED(component)
+
+        auto logLevel = QEverCloudLogLevelToQuentierLogLevel(level);
+        return QuentierIsLogLevelActive(logLevel);
+    }
+
+    virtual void log(
+        const qevercloud::LogLevel level, const char * component,
+        const char * fileName, const quint32 lineNumber, const qint64 timestamp,
+        const QString & message) override
+    {
+        Q_UNUSED(component)
+        Q_UNUSED(timestamp)
+
+        QuentierAddLogEntry(
+            QString::fromUtf8(fileName),
+            static_cast<int>(lineNumber),
+            message,
+            QEverCloudLogLevelToQuentierLogLevel(level));
+    }
+
+    virtual void setLevel(const qevercloud::LogLevel level) override
+    {
+        Q_UNUSED(level)
+
+        throw std::runtime_error(
+            "Unimplemented method QEverCloudLogger::setLevel was called");
+    }
+
+    virtual qevercloud::LogLevel level() const override
+    {
+        auto logLevel = QuentierMinLogLevel();
+        switch(logLevel)
+        {
+        case LogLevel::TraceLevel:
+            return qevercloud::LogLevel::Trace;
+        case LogLevel::DebugLevel:
+            return qevercloud::LogLevel::Debug;
+        case LogLevel::InfoLevel:
+            return qevercloud::LogLevel::Info;
+        case LogLevel::WarnLevel:
+            return qevercloud::LogLevel::Warn;
+        case LogLevel::ErrorLevel:
+            return qevercloud::LogLevel::Error;
+        default:
+            return qevercloud::LogLevel::Info;
+        }
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////
 
 void QuentierInitializeLogging()
 {
     QuentierLogger & logger = QuentierLogger::instance();
     Q_UNUSED(logger)
+
+    qevercloud::setLogger(std::make_shared<QEverCloudLogger>());
 }
 
 void QuentierAddLogEntry(const QString & sourceFileName, const int sourceFileLineNumber,
                          const QString & message, const LogLevel::type logLevel)
 {
     QString relativeSourceFileName = sourceFileName;
-    int prefixIndex = relativeSourceFileName.indexOf(QStringLiteral("libquentier"),
-                                                     Qt::CaseInsensitive);
-    if (prefixIndex >= 0) {
+
+    int prefixIndex = relativeSourceFileName.indexOf(
+        QStringLiteral("libquentier"),
+        Qt::CaseInsensitive);
+
+    if (prefixIndex < 0) {
+        prefixIndex = relativeSourceFileName.indexOf(
+            QStringLiteral("QEverCloud"),
+            Qt::CaseInsensitive);
+    }
+
+    if (prefixIndex >= 0)
+    {
         relativeSourceFileName.remove(0, prefixIndex);
     }
     else {
         QString appName = QCoreApplication::applicationName().toLower();
-        prefixIndex = relativeSourceFileName.indexOf(appName, Qt::CaseInsensitive);
+        prefixIndex = relativeSourceFileName.indexOf(
+            appName,
+            Qt::CaseInsensitive);
+
         if (prefixIndex >= 0) {
             relativeSourceFileName.remove(0, prefixIndex + appName.size() + 1);
         }
@@ -111,35 +213,6 @@ void QuentierRestartLogging()
 {
     QuentierLogger & logger = QuentierLogger::instance();
     logger.restartLogging();
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-QDebug & operator<<(QDebug & dbg, const LogLevel logLevel)
-{
-    switch(logLevel)
-    {
-    case LogLevel::Trace:
-        dbg << "Trace";
-        break;
-    case LogLevel::Debug:
-        dbg << "Debug";
-        break;
-    case LogLevel::Info:
-        dbg << "Info";
-        break;
-    case LogLevel::Warning:
-        dbg << "Warning";
-        break;
-    case LogLevel::Error:
-        dbg << "Error";
-        break;
-    default:
-        dbg << "Unknown (" << static_cast<qint64>(logLevel) << ")";
-        break;
-    }
-
-    return dbg;
 }
 
 } // namespace quentier
