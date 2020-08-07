b69f9f60b19dbd33021bc516d86d03bf332de3cc
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2020-08-07 13:40:47 +0300

    Issue #134: add Doxygen documentation to logging functions

--- headers/quentier/logging/QuentierLogger.h
+++ headers/quentier/logging/QuentierLogger.h
@@ -28,6 +28,10 @@
 
 namespace quentier {
 
+/**
+ * The LogLevel enumeration defines different levels for log entries which are
+ * meant to separate log entries with different importance and meaning
+ */
 enum class LogLevel
 {
     Trace,
@@ -42,8 +46,18 @@ QUENTIER_EXPORT QDebug & operator<<(QDebug & dbg, const LogLevel logLevel);
 QUENTIER_EXPORT QTextStream & operator<<(
     QTextStream & strm, const LogLevel logLevel);
 
+/**
+ * This function needs to be called once during a process lifetime before
+ * libquentier is used by the process. It initializes some internal data
+ * structures used by libquentier's logging subsystem and prepares to write logs
+ * to rotated files in directory path to which is returned by
+ * QuentierLogFilesDirPath function.
+ */
 void QUENTIER_EXPORT QuentierInitializeLogging();
 
+/**
+ * This function is used to add new log entry to logs written by libquentier
+ */
 void QUENTIER_EXPORT QuentierAddLogEntry(
     const QString & sourceFileName,
     const int sourceFileLineNumber,
@@ -51,16 +65,38 @@ void QUENTIER_EXPORT QuentierAddLogEntry(
     const QString & message,
     const LogLevel logLevel);
 
+/**
+ * Current minimal log level used by libquentier. By default minimal log level
+ * is LogLevel::Info which means that Info, Warning and Error logs are being
+ * output but Debug and Trace ones are not
+ */
 LogLevel QUENTIER_EXPORT QuentierMinLogLevel();
 
+/**
+ * Change the current minimal log level used by libquentier
+ */
 void QUENTIER_EXPORT QuentierSetMinLogLevel(const LogLevel logLevel);
 
+/**
+ * Call this function to write logs not only to rotating files but also to
+ * stdout
+ */
 void QUENTIER_EXPORT QuentierAddStdOutLogDestination();
 
+/**
+ * Check whether log level is active i.e. whether log level is larger than or
+ * equal to the minimal log level
+ */
 bool QUENTIER_EXPORT QuentierIsLogLevelActive(const LogLevel logLevel);
 
+/**
+ * Directory containing rotating log files written by libquentier
+ */
 QString QUENTIER_EXPORT QuentierLogFilesDirPath();
 
+/**
+ * Clear logs accumulated within the existing log file
+ */
 void QUENTIER_EXPORT QuentierRestartLogging();
 
 } // namespace quentier
