ab1798c3924272eedf715ff213fcc06aecd9e581
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2020-12-03 16:45:05 +0300

    Add custom libxml2 target to file installed along with libquentier

--- cmake/modules/LibquentierFindOtherDependenciesQt5.cmake
+++ cmake/modules/LibquentierFindOtherDependenciesQt5.cmake
@@ -8,3 +8,14 @@ LibquentierFindPackageWrapper(QEverCloud-qt5 ${LIBQUENTIER_FIND_PACKAGE_ARG})
 LibquentierFindPackageWrapper(Qt5Keychain ${LIBQUENTIER_FIND_PACKAGE_ARG})
 LibquentierFindPackageWrapper(LibXml2 ${LIBQUENTIER_FIND_PACKAGE_ARG})
 LibquentierFindPackageWrapper(OpenSSL ${LIBQUENTIER_FIND_PACKAGE_ARG})
+
+add_library(Libxml2::Libxml2 SHARED IMPORTED)
+if(MSVC)
+  set_target_properties(Libxml2::Libxml2 PROPERTIES
+    IMPORTED_IMPLIB ${LIBXML2_LIBRARIES}
+  )
+else()
+  set_target_properties(Libxml2::Libxml2 PROPERTIES
+    IMPORTED_LOCATION ${LIBXML2_LIBRARIES}
+  )
+endif()
