7f7dbc71db8396a6b34d952274c510f679c3dc3b
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2020-12-03 16:28:56 +0300

    Move custom target for Libxml2 into a namespace

--- cmake/modules/LibquentierFindLibXml2.cmake
+++ cmake/modules/LibquentierFindLibXml2.cmake
@@ -3,13 +3,13 @@ if(LIBXML2_FOUND)
   include_directories(${LIBXML2_INCLUDE_DIR})
   get_filename_component(LIBXML2_LIB_DIR "${LIBXML2_LIBRARIES}" PATH)
 
-  add_library(Libxml2 SHARED IMPORTED)
+  add_library(Libxml2::Libxml2 SHARED IMPORTED)
   if(MSVC)
-    set_target_properties(Libxml2 PROPERTIES
+    set_target_properties(Libxml2::Libxml2 PROPERTIES
       IMPORTED_IMPLIB ${LIBXML2_LIBRARIES}
     )
   else()
-    set_target_properties(Libxml2 PROPERTIES
+    set_target_properties(Libxml2::Libxml2 PROPERTIES
       IMPORTED_LOCATION ${LIBXML2_LIBRARIES}
     )
   endif()
