4669c0eaf17368a7e4edc202cbb41d8fd592339a
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2021-04-28 22:35:42 +0300

    Add new header with template functions for sending arbitrary functions to QObjects and/or QThreads to execute in their context

---
+++ headers/quentier/utility/Threading.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2021 Dmitry Ivanov
+ *
+ * This file is part of libquentier
+ *
+ * libquentier is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * libquentier is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libquentier. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <QAbstractEventDispatcher>
+#include <QObject>
+
+#if QT_VERSION >= QT_VERSION_CHECK(5, 10, 0)
+#include <QMetaObject>
+#else
+#include <QThread>
+#endif
+
+namespace quentier::utility {
+
+template <typename Function>
+void postToObject(QObject * pObject, Function && function)
+{
+    Q_ASSERT(pObject);
+
+#if QT_VERSION >= QT_VERSION_CHECK(5, 10, 0)
+    QMetaObject::invoke(pObject, std::forward<Function>(function));
+#else
+    QObject src;
+    QObject::connect(
+        &src, &QObject::destroyed, pObject, std::forward<Function>(function),
+        Qt::QueuedConnection);
+#endif
+}
+
+template <typename Function>
+void postToThread(QThread * pThread, Function && function)
+{
+    Q_ASSERT(pThread);
+
+    QObject * pObject = QAbstractEventDispatcher::instance(pThread);
+    Q_ASSERT(pObject);
+
+#if QT_VERSION >= QT_VERSION_CHECK(5, 10, 0)
+    QMetaObject::invokeMethod(pObject, std::forward<Function>(function));
+#else
+    QObject src;
+    QObject::connect(
+        &src, &QObject::destroyed, pObject, std::forward<Function>(function),
+        Qt::QueuedConnection);
+#endif
+}
+
+} // namespace quentier::utility
