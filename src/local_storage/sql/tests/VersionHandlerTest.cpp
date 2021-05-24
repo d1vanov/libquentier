466f1b0e269ff6efbcd11c377df2fb2b5f4f86c5
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2021-05-24 17:32:14 +0300

    Add more unit tests for VersionHandler

--- src/local_storage/sql/tests/VersionHandlerTest.cpp
+++ src/local_storage/sql/tests/VersionHandlerTest.cpp
@@ -19,6 +19,7 @@
 #include "../ConnectionPool.h"
 #include "../TablesInitializer.h"
 #include "../VersionHandler.h"
+#include "../patches/Patch1To2.h"
 
 #include <quentier/exception/IQuentierException.h>
 
@@ -26,6 +27,7 @@
 
 #include <QCoreApplication>
 #include <QSqlDatabase>
+#include <QSqlQuery>
 #include <QThreadPool>
 
 namespace quentier::local_storage::sql::tests {
@@ -134,6 +136,83 @@ TEST_F(VersionHandlerTest, HandleEmptyNewlyCreatedDatabase)
     auto versionFuture = versionHandler->version();
     versionFuture.waitForFinished();
     EXPECT_EQ(versionFuture.result(), 2);
+
+    auto highestSupportedVersionFuture =
+        versionHandler->highestSupportedVersion();
+    highestSupportedVersionFuture.waitForFinished();
+    EXPECT_EQ(highestSupportedVersionFuture.result(), 2);
+}
+
+TEST_F(VersionHandlerTest, HandleDatabaseOfVersion1)
+{
+    {
+        auto database = m_connectionPool->database();
+        QSqlQuery query{database};
+        const bool res = query.exec(QStringLiteral(
+            "UPDATE Auxiliary SET version = 1 WHERE version = 2"));
+        EXPECT_TRUE(res);
+    }
+
+    const auto versionHandler = std::make_shared<VersionHandler>(
+        m_account, m_connectionPool, QThreadPool::globalInstance(),
+        m_writerThread);
+
+    auto isVersionTooHighFuture = versionHandler->isVersionTooHigh();
+    isVersionTooHighFuture.waitForFinished();
+    EXPECT_FALSE(isVersionTooHighFuture.result());
+
+    auto requiresUpgradeFuture = versionHandler->requiresUpgrade();
+    requiresUpgradeFuture.waitForFinished();
+    EXPECT_TRUE(requiresUpgradeFuture.result());
+
+    auto requiredPatchesFuture = versionHandler->requiredPatches();
+    requiredPatchesFuture.waitForFinished();
+    const auto requiredPatches = requiredPatchesFuture.result();
+    EXPECT_EQ(requiredPatches.size(), 1);
+
+    const auto patch1to2 =
+        std::dynamic_pointer_cast<Patch1To2>(requiredPatches[0]);
+    EXPECT_TRUE(patch1to2);
+
+    auto versionFuture = versionHandler->version();
+    versionFuture.waitForFinished();
+    EXPECT_EQ(versionFuture.result(), 1);
+
+    auto highestSupportedVersionFuture =
+        versionHandler->highestSupportedVersion();
+    highestSupportedVersionFuture.waitForFinished();
+    EXPECT_EQ(highestSupportedVersionFuture.result(), 2);
+}
+
+TEST_F(VersionHandlerTest, HandleDatabaseOfTooHighVersion)
+{
+    {
+        auto database = m_connectionPool->database();
+        QSqlQuery query{database};
+        const bool res = query.exec(QStringLiteral(
+            "UPDATE Auxiliary SET version = 999 WHERE version = 2"));
+        EXPECT_TRUE(res);
+    }
+
+    const auto versionHandler = std::make_shared<VersionHandler>(
+        m_account, m_connectionPool, QThreadPool::globalInstance(),
+        m_writerThread);
+
+    auto isVersionTooHighFuture = versionHandler->isVersionTooHigh();
+    isVersionTooHighFuture.waitForFinished();
+    EXPECT_TRUE(isVersionTooHighFuture.result());
+
+    auto requiresUpgradeFuture = versionHandler->requiresUpgrade();
+    requiresUpgradeFuture.waitForFinished();
+    EXPECT_FALSE(requiresUpgradeFuture.result());
+
+    auto requiredPatchesFuture = versionHandler->requiredPatches();
+    requiredPatchesFuture.waitForFinished();
+    EXPECT_TRUE(requiredPatchesFuture.result().isEmpty());
+
+    auto versionFuture = versionHandler->version();
+    versionFuture.waitForFinished();
+    EXPECT_EQ(versionFuture.result(), 999);
 
     auto highestSupportedVersionFuture =
         versionHandler->highestSupportedVersion();
