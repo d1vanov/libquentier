3867e239148394903ee426f4169fe5ff3e2008d1
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2021-06-02 19:23:59 +0300

    Add more unit tests for UsersHandler

--- src/local_storage/sql/tests/UsersHandlerTest.cpp
+++ src/local_storage/sql/tests/UsersHandlerTest.cpp
@@ -33,7 +33,75 @@ namespace quentier::local_storage::sql::tests {
 
 namespace {
 
-const QString gTestAccountName = QStringLiteral("testAccountName");
+[[nodiscard]] qevercloud::User createUser()
+{
+    qevercloud::User user;
+    user.setId(1);
+    user.setUsername(QStringLiteral("fake_user_username"));
+    user.setEmail(QStringLiteral("fake_user _mail"));
+    user.setName(QStringLiteral("fake_user_name"));
+    user.setTimezone(QStringLiteral("fake_user_timezone"));
+    user.setPrivilege(qevercloud::PrivilegeLevel::NORMAL);
+    user.setCreated(2);
+    user.setUpdated(3);
+    user.setActive(true);
+    return user;
+}
+
+[[nodiscard]] qevercloud::UserAttributes createUserAttributes()
+{
+    qevercloud::UserAttributes userAttributes;
+
+    userAttributes.setDefaultLocationName(
+        QStringLiteral("defaultLocationName"));
+
+    userAttributes.setDefaultLatitude(55.0);
+    userAttributes.setDefaultLongitude(36.0);
+    userAttributes.setPreactivation(false);
+
+    userAttributes.setViewedPromotions(
+        QStringList{} << QStringLiteral("promotion1")
+        << QStringLiteral("promotion2"));
+
+    userAttributes.setIncomingEmailAddress(QStringLiteral("example@mail.com"));
+
+    userAttributes.setRecentMailedAddresses(
+        QStringList{} << QStringLiteral("recentMailedAddress1@example.com")
+        << QStringLiteral("recentMailedAddress2@example.com"));
+
+    userAttributes.setComments(QStringLiteral("comments"));
+    userAttributes.setDateAgreedToTermsOfService(qevercloud::Timestamp{2});
+    userAttributes.setMaxReferrals(32);
+    userAttributes.setReferralCount(10);
+    userAttributes.setRefererCode(QStringLiteral("refererCode"));
+    userAttributes.setSentEmailDate(qevercloud::Timestamp{3});
+    userAttributes.setSentEmailCount(20);
+    userAttributes.setDailyEmailLimit(40);
+    userAttributes.setEmailOptOutDate(qevercloud::Timestamp{4});
+    userAttributes.setPartnerEmailOptInDate(qevercloud::Timestamp{5});
+    userAttributes.setPreferredLanguage(QStringLiteral("En"));
+    userAttributes.setPreferredCountry(QStringLiteral("New Zealand"));
+    userAttributes.setClipFullPage(true);
+    userAttributes.setTwitterUserName(QStringLiteral("twitterUserName"));
+    userAttributes.setTwitterId(QStringLiteral("twitterId"));
+    userAttributes.setGroupName(QStringLiteral("groupName"));
+    userAttributes.setRecognitionLanguage(QStringLiteral("Ru"));
+    userAttributes.setReferralProof(QStringLiteral("referralProof"));
+    userAttributes.setEducationalDiscount(false);
+    userAttributes.setBusinessAddress(QStringLiteral("business@example.com"));
+    userAttributes.setHideSponsorBilling(true);
+    userAttributes.setUseEmailAutoFiling(true);
+
+    userAttributes.setReminderEmailConfig(
+        qevercloud::ReminderEmailConfig::DO_NOT_SEND);
+
+    userAttributes.setEmailAddressLastConfirmed(qevercloud::Timestamp{6});
+    userAttributes.setPasswordUpdated(qevercloud::Timestamp{7});
+    userAttributes.setSalesforcePushEnabled(false);
+    userAttributes.setShouldLogClientEvent(false);
+
+    return userAttributes;
+}
 
 class UsersHandlerTest : public testing::Test
 {
@@ -98,6 +166,77 @@ TEST_F(UsersHandlerTest, CtorNullWriterThread)
         const auto usersHandler = std::make_shared<UsersHandler>(
             m_connectionPool, QThreadPool::globalInstance(), nullptr),
         IQuentierException);
+}
+
+TEST_F(UsersHandlerTest, ShouldHaveZeroUserCountWhenThereAreNoUsers)
+{
+    const auto usersHandler = std::make_shared<UsersHandler>(
+        m_connectionPool, QThreadPool::globalInstance(), m_writerThread);
+
+    auto userCountFuture = usersHandler->userCount();
+    userCountFuture.waitForFinished();
+    EXPECT_EQ(userCountFuture.result(), 0U);
+}
+
+TEST_F(UsersHandlerTest, ShouldNotFindNonexistentUser)
+{
+    const auto usersHandler = std::make_shared<UsersHandler>(
+        m_connectionPool, QThreadPool::globalInstance(), m_writerThread);
+
+    auto userFuture = usersHandler->findUserById(qevercloud::UserID{1});
+    userFuture.waitForFinished();
+    EXPECT_EQ(userFuture.resultCount(), 0);
+}
+
+TEST_F(UsersHandlerTest, IgnoreAttemptToExpungeNonexistentUser)
+{
+    const auto usersHandler = std::make_shared<UsersHandler>(
+        m_connectionPool, QThreadPool::globalInstance(), m_writerThread);
+
+    auto expungeUserFuture = usersHandler->findUserById(qevercloud::UserID{1});
+    EXPECT_NO_THROW(expungeUserFuture.waitForFinished());
+}
+
+TEST_F(UsersHandlerTest, PutNewUser)
+{
+    const auto usersHandler = std::make_shared<UsersHandler>(
+        m_connectionPool, QThreadPool::globalInstance(), m_writerThread);
+
+    const auto user = createUser();
+    auto putUserFuture = usersHandler->putUser(user);
+    EXPECT_NO_THROW(putUserFuture.waitForFinished());
+
+    auto userCountFuture = usersHandler->userCount();
+    userCountFuture.waitForFinished();
+    EXPECT_EQ(userCountFuture.result(), 1U);
+
+    auto foundUserFuture = usersHandler->findUserById(*user.id());
+    foundUserFuture.waitForFinished();
+    const auto foundUser = foundUserFuture.result();
+    EXPECT_TRUE(foundUser);
+    EXPECT_EQ(*foundUser, user);
+}
+
+TEST_F(UsersHandlerTest, PutNewUserWithUserAttributes)
+{
+    const auto usersHandler = std::make_shared<UsersHandler>(
+        m_connectionPool, QThreadPool::globalInstance(), m_writerThread);
+
+    auto user = createUser();
+    user.setAttributes(createUserAttributes());
+
+    auto putUserFuture = usersHandler->putUser(user);
+    EXPECT_NO_THROW(putUserFuture.waitForFinished());
+
+    auto userCountFuture = usersHandler->userCount();
+    userCountFuture.waitForFinished();
+    EXPECT_EQ(userCountFuture.result(), 1U);
+
+    auto foundUserFuture = usersHandler->findUserById(*user.id());
+    foundUserFuture.waitForFinished();
+    const auto foundUser = foundUserFuture.result();
+    EXPECT_TRUE(foundUser);
+    EXPECT_EQ(*foundUser, user);
 }
 
 } // namespace quentier::local_storage::sql::tests
