c4ded691e731eb9748af015fa87b7d8ab4b8c915
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2022-01-31 18:42:29 +0300

    Continue to implement SyncChunksStorage

--- src/synchronization/sync_chunks/SyncChunksStorage.cpp
+++ src/synchronization/sync_chunks/SyncChunksStorage.cpp
@@ -22,7 +22,12 @@
 #include <quentier/exception/RuntimeError.h>
 #include <quentier/logging/QuentierLogger.h>
 
+#include <qevercloud/serialization/json/SyncChunk.h>
+
+#include <QFile>
 #include <QFileInfo>
+#include <QJsonDocument>
+#include <QTextStream>
 
 namespace quentier::synchronization {
 
@@ -61,9 +66,45 @@ namespace {
 [[nodiscard]] std::optional<qevercloud::SyncChunk> deserializeSyncChunk(
     const QString & filePath)
 {
-    // TODO: implement
-    Q_UNUSED(filePath)
-    return std::nullopt;
+    QFile file{filePath};
+    if (Q_UNLIKELY(!file.open(QIODevice::ReadOnly))) {
+        QNWARNING(
+            "synchronization::SyncChunksStorage",
+            "Failed to open serialized sync chunk file: " << filePath);
+        return std::nullopt;
+    }
+
+    const QByteArray contents = file.readAll();
+    file.close();
+
+    QJsonParseError error;
+    QJsonDocument doc = QJsonDocument::fromJson(contents, &error);
+    if (Q_UNLIKELY(doc.isNull())) {
+        QNWARNING(
+            "synchronization::SyncChunksStorage",
+            "Failed to parse serialized sync chunk from file to json document: "
+                << error.errorString() << "; file: " << filePath);
+        return std::nullopt;
+    }
+
+    if (Q_UNLIKELY(!doc.isObject())) {
+        QNWARNING(
+            "synchronization::SyncChunksStorage",
+            "Cannot parse serialized sync chunk: json is not an object; file: "
+                << filePath);
+        return std::nullopt;
+    }
+
+    const QJsonObject obj = doc.object();
+    qevercloud::SyncChunk syncChunk;
+    if (!qevercloud::deserializeFromJson(obj, syncChunk)) {
+        QNWARNING(
+            "synchronization::SyncChunksStorage",
+            "Failed to deserialize sync chunk from json object, file: "
+                << filePath);
+    }
+
+    return syncChunk;
 }
 
 [[nodiscard]] QList<qevercloud::SyncChunk> fetchRelevantSyncChunks(
@@ -117,6 +158,92 @@ namespace {
     }
 
     return result;
+}
+
+[[nodiscard]] std::optional<qint32> syncChunkLowUsn(
+    const qevercloud::SyncChunk & syncChunk)
+{
+    std::optional<qint32> lowUsn;
+
+    const auto checkLowUsn = [&](const auto & items)
+    {
+        for (const auto & item: qAsConst(items)) {
+            if (item.updateSequenceNum() &&
+                (!lowUsn || *lowUsn > *item.updateSequenceNum()))
+            {
+                lowUsn = *item.updateSequenceNum();
+            }
+        }
+    };
+
+    if (syncChunk.notes()) {
+        checkLowUsn(*syncChunk.notes());
+    }
+
+    if (syncChunk.notebooks()) {
+        checkLowUsn(*syncChunk.notebooks());
+    }
+
+    if (syncChunk.tags()) {
+        checkLowUsn(*syncChunk.tags());
+    }
+
+    if (syncChunk.searches()) {
+        checkLowUsn(*syncChunk.searches());
+    }
+
+    if (syncChunk.resources()) {
+        checkLowUsn(*syncChunk.resources());
+    }
+
+    if (syncChunk.linkedNotebooks()) {
+        checkLowUsn(*syncChunk.linkedNotebooks());
+    }
+
+    return lowUsn;
+}
+
+void putSyncChunks(
+    const QDir & dir, const QList<qevercloud::SyncChunk> & syncChunks)
+{
+    for (const auto & syncChunk: qAsConst(syncChunks)) {
+        const auto lowUsn = syncChunkLowUsn(syncChunk);
+        const auto highUsn = syncChunk.chunkHighUSN();
+        if (!lowUsn || !highUsn) {
+            QNWARNING(
+                "synchronization::SyncChunksStorage",
+                "Failed to fetch low and/or high USN for sync chunk: "
+                    << syncChunk);
+            continue;
+        }
+
+        const QString fileName = [&]
+        {
+            QString fileName;
+            QTextStream strm{&fileName};
+
+            strm << *lowUsn;
+            strm << "_";
+            strm << *highUsn;
+            return fileName;
+        }();
+
+        QFile syncChunkFile{dir.absoluteFilePath(fileName)};
+        if (!syncChunkFile.open(QIODevice::WriteOnly)) {
+            QNWARNING(
+                "synchronization::SyncChunksStorage",
+                "Failed to open file to save sync chunk into: "
+                    << dir.absoluteFilePath(fileName));
+            continue;
+        }
+
+        const QJsonObject obj = qevercloud::serializeToJson(syncChunk);
+        QJsonDocument doc;
+        doc.setObject(obj);
+
+        syncChunkFile.write(doc.toJson(QJsonDocument::Indented));
+        syncChunkFile.close();
+    }
 }
 
 } // namespace
@@ -202,17 +329,35 @@ QList<qevercloud::SyncChunk>
 void SyncChunksStorage::putUserOwnSyncChunks(
     const QList<qevercloud::SyncChunk> & syncChunks)
 {
-    // TODO: implement
-    Q_UNUSED(syncChunks)
+    putSyncChunks(m_userOwnSyncChunksDir, syncChunks);
 }
 
 void SyncChunksStorage::putLinkedNotebookSyncChunks(
     const qevercloud::Guid & linkedNotebookGuid,
     const QList<qevercloud::SyncChunk> & syncChunks)
 {
-    // TODO: implement
-    Q_UNUSED(linkedNotebookGuid)
-    Q_UNUSED(syncChunks)
+    const QDir linkedNotebookDir{
+        m_rootDir.absoluteFilePath(linkedNotebookGuid)};
+
+    const QFileInfo linkedNotebookDirInfo{linkedNotebookDir.absolutePath()};
+    if (!linkedNotebookDirInfo.exists()) {
+        if (!m_rootDir.mkpath(linkedNotebookDir.absolutePath())) {
+            QNWARNING(
+                "synchronization::SyncChunksStorage",
+                "Failed to create dir to store linked notebook sync chunks: "
+                    << linkedNotebookDir.absolutePath());
+            return;
+        }
+    }
+    else if (!linkedNotebookDirInfo.isWritable()) {
+        QNWARNING(
+            "synchronization::SyncChunksStorage",
+            "Dir to store linked notebook sync chunks is not writable: "
+                << linkedNotebookDir.absolutePath());
+        return;
+    }
+
+    putSyncChunks(linkedNotebookDir, syncChunks);
 }
 
 void SyncChunksStorage::clearUserOwnSyncChunks()
