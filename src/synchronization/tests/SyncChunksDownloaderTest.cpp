d31d0273f3e3b65e5c05077270377676ae2536f6
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2022-02-20 21:45:12 +0300

    Add some more unit tests for SyncChunksDownloader

--- src/synchronization/tests/SyncChunksDownloaderTest.cpp
+++ src/synchronization/tests/SyncChunksDownloaderTest.cpp
@@ -29,13 +29,118 @@
 
 #include <gtest/gtest.h>
 
+#include <array>
 #include <memory>
 
+// clazy:excludeall=non-pod-global-static
 // clazy:excludeall=returning-void-expression
 
 namespace quentier::synchronization::tests {
 
-using ::testing::Return;
+namespace {
+
+const auto sampleSyncChunk1 =
+    qevercloud::SyncChunkBuilder{}
+        .setNotebooks(
+            QList<qevercloud::Notebook>{}
+            << qevercloud::NotebookBuilder{}
+                   .setGuid(UidGenerator::Generate())
+                   .setName(QStringLiteral("Notebook #1"))
+                   .setUpdateSequenceNum(0)
+                   .build()
+            << qevercloud::NotebookBuilder{}
+                   .setGuid(UidGenerator::Generate())
+                   .setName(QStringLiteral("Notebook #2"))
+                   .setUpdateSequenceNum(35)
+                   .build())
+        .setChunkHighUSN(35)
+        .setUpdateCount(35)
+        .build();
+
+const auto sampleSyncChunk2 =
+    qevercloud::SyncChunkBuilder{}
+        .setNotebooks(
+            QList<qevercloud::Notebook>{}
+            << qevercloud::NotebookBuilder{}
+                   .setGuid(UidGenerator::Generate())
+                   .setName(QStringLiteral("Notebook #3"))
+                   .setUpdateSequenceNum(36)
+                   .build()
+            << qevercloud::NotebookBuilder{}
+                   .setGuid(UidGenerator::Generate())
+                   .setName(QStringLiteral("Notebook #4"))
+                   .setUpdateSequenceNum(54)
+                   .build())
+        .setChunkHighUSN(54)
+        .setUpdateCount(54)
+        .build();
+
+const auto sampleSyncChunk3 =
+    qevercloud::SyncChunkBuilder{}
+        .setNotebooks(
+            QList<qevercloud::Notebook>{}
+            << qevercloud::NotebookBuilder{}
+                   .setGuid(UidGenerator::Generate())
+                   .setName(QStringLiteral("Notebook #5"))
+                   .setUpdateSequenceNum(55)
+                   .build()
+            << qevercloud::NotebookBuilder{}
+                   .setGuid(UidGenerator::Generate())
+                   .setName(QStringLiteral("Notebook #6"))
+                   .setUpdateSequenceNum(82)
+                   .build())
+        .setChunkHighUSN(82)
+        .setUpdateCount(82)
+        .build();
+
+const auto sampleFullSyncSyncChunkFilter =
+    qevercloud::SyncChunkFilterBuilder{}
+        .setIncludeNotebooks(true)
+        .setIncludeNotes(true)
+        .setIncludeTags(true)
+        .setIncludeSearches(true)
+        .setIncludeNoteResources(true)
+        .setIncludeNoteAttributes(true)
+        .setIncludeNoteApplicationDataFullMap(true)
+        .setIncludeNoteResourceApplicationDataFullMap(true)
+        .setIncludeLinkedNotebooks(true)
+        .build();
+
+const auto sampleIncrementalSyncSyncChunkFilter =
+    qevercloud::SyncChunkFilterBuilder{}
+        .setIncludeNotebooks(true)
+        .setIncludeNotes(true)
+        .setIncludeTags(true)
+        .setIncludeSearches(true)
+        .setIncludeNoteResources(true)
+        .setIncludeNoteAttributes(true)
+        .setIncludeNoteApplicationDataFullMap(true)
+        .setIncludeNoteResourceApplicationDataFullMap(true)
+        .setIncludeLinkedNotebooks(true)
+        .setIncludeExpunged(true)
+        .setIncludeResources(true)
+        .build();
+
+[[nodiscard]] QList<qevercloud::SyncChunk> adjustSyncChunksUpdateCounts(
+    QList<qevercloud::SyncChunk> syncChunks)
+{
+    if (Q_UNLIKELY(syncChunks.isEmpty())) {
+        return {};
+    }
+
+    const auto & lastSyncChunk = syncChunks.constLast();
+    const qint32 updateCount = lastSyncChunk.updateCount();
+    for (auto & syncChunk: syncChunks) {
+        syncChunk.setUpdateCount(updateCount);
+    }
+
+    return syncChunks;
+}
+
+} // namespace
+
+using testing::InSequence;
+using testing::Return;
 
 class SyncChunksDownloaderTest : public ::testing::Test
 {
@@ -57,58 +162,108 @@ TEST_F(SyncChunksDownloaderTest, CtorNullNoteStore)
         InvalidArgument);
 }
 
-TEST_F(SyncChunksDownloaderTest, DownloadSingleUserOwnSyncChunkWithFullSync)
+struct UserOwnSyncChunksTestData
 {
-    SyncChunksDownloader downloader{SynchronizationMode::Full, m_mockNoteStore};
+    QString m_testName;
+    SynchronizationMode m_syncMode = SynchronizationMode::Full;
+    QList<qevercloud::SyncChunk> m_syncChunks;
+    qevercloud::SyncChunkFilter m_syncChunkFilter;
+};
+
+class SyncChunksDownloaderUserOwnSyncChunksTest :
+    public SyncChunksDownloaderTest,
+    public testing::WithParamInterface<UserOwnSyncChunksTestData>
+{};
+
+std::array gUserOwnSyncChunksTestData{
+    UserOwnSyncChunksTestData{
+        QStringLiteral("Single user own sync chunk with full sync"),
+        SynchronizationMode::Full,
+        QList<qevercloud::SyncChunk>{} << sampleSyncChunk1,
+        sampleFullSyncSyncChunkFilter,
+    },
+    UserOwnSyncChunksTestData{
+        QStringLiteral("Single user own sync chunk with incremental sync"),
+        SynchronizationMode::Incremental,
+        QList<qevercloud::SyncChunk>{} << sampleSyncChunk1,
+        sampleIncrementalSyncSyncChunkFilter,
+    },
+    UserOwnSyncChunksTestData{
+        QStringLiteral("Multiple user own sync chunks with full sync"),
+        SynchronizationMode::Full,
+        adjustSyncChunksUpdateCounts(
+            QList<qevercloud::SyncChunk>{}
+            << sampleSyncChunk1 << sampleSyncChunk2 << sampleSyncChunk3),
+        sampleFullSyncSyncChunkFilter,
+    },
+    UserOwnSyncChunksTestData{
+        QStringLiteral("Multiple user own sync chunks with incremental sync"),
+        SynchronizationMode::Incremental,
+        adjustSyncChunksUpdateCounts(
+            QList<qevercloud::SyncChunk>{}
+            << sampleSyncChunk1 << sampleSyncChunk2 << sampleSyncChunk3),
+        sampleIncrementalSyncSyncChunkFilter,
+    },
+};
+
+INSTANTIATE_TEST_SUITE_P(
+    SyncChunksDownloaderUserOwnSyncChunksTestInstance,
+    SyncChunksDownloaderUserOwnSyncChunksTest,
+    testing::ValuesIn(gUserOwnSyncChunksTestData));
+
+TEST_P(SyncChunksDownloaderUserOwnSyncChunksTest, DownloadUserOwnSyncChunks)
+{
+    const auto & testData = GetParam();
+
+    SyncChunksDownloader downloader{testData.m_syncMode, m_mockNoteStore};
 
     const QString authToken = QStringLiteral("token");
     auto ctx = qevercloud::newRequestContext(authToken);
 
-    const auto syncChunk =
-        qevercloud::SyncChunkBuilder{}
-            .setNotebooks(
-                QList<qevercloud::Notebook>{}
-                << qevercloud::NotebookBuilder{}
-                       .setGuid(UidGenerator::Generate())
-                       .setName(QStringLiteral("Notebook #1"))
-                       .setUpdateSequenceNum(0)
-                       .build()
-                << qevercloud::NotebookBuilder{}
-                       .setGuid(UidGenerator::Generate())
-                       .setName(QStringLiteral("Notebook #2"))
-                       .setUpdateSequenceNum(35)
-                       .build())
-            .setChunkHighUSN(35)
-            .setUpdateCount(35)
-            .build();
-
-    const auto syncChunkFilter =
-        qevercloud::SyncChunkFilterBuilder{}
-            .setIncludeNotebooks(true)
-            .setIncludeNotes(true)
-            .setIncludeTags(true)
-            .setIncludeSearches(true)
-            .setIncludeNoteResources(true)
-            .setIncludeNoteAttributes(true)
-            .setIncludeNoteApplicationDataFullMap(true)
-            .setIncludeNoteResourceApplicationDataFullMap(true)
-            .setIncludeLinkedNotebooks(true)
-            .build();
-
-    constexpr qint32 afterUsn = 0;
+    constexpr qint32 afterUsnInitial = 0;
     constexpr qint32 maxEntries = 50;
-    EXPECT_CALL(
-        *m_mockNoteStore,
-        getFilteredSyncChunkAsync(afterUsn, maxEntries, syncChunkFilter, ctx))
-        .WillOnce(Return(threading::makeReadyFuture(syncChunk)));
+    qint32 afterUsn = afterUsnInitial;
 
-    const auto syncChunksFuture = downloader.downloadSyncChunks(afterUsn, ctx);
-    ASSERT_TRUE(syncChunksFuture.isFinished());
-    ASSERT_EQ(syncChunksFuture.resultCount(), 1);
+    InSequence s;
+
+    std::optional<qint32> previousChunkHighUsn;
+    for (const auto & syncChunk: qAsConst(testData.m_syncChunks)) {
+        if (previousChunkHighUsn) {
+            afterUsn = *previousChunkHighUsn;
+        }
+
+        EXPECT_CALL(*m_mockNoteStore, getFilteredSyncChunkAsync)
+            .WillOnce([&, afterUsnCurrent = afterUsn](
+                          const qint32 afterUsnParam,
+                          const qint32 maxEntriesParam,
+                          const qevercloud::SyncChunkFilter & syncChunkFilter,
+                          const qevercloud::IRequestContextPtr & ctxParam) {
+                EXPECT_EQ(afterUsnParam, afterUsnCurrent);
+                EXPECT_EQ(maxEntriesParam, maxEntries);
+                EXPECT_EQ(syncChunkFilter, testData.m_syncChunkFilter);
+                EXPECT_EQ(ctxParam, ctx);
+                return threading::makeReadyFuture(syncChunk);
+            });
+
+        ASSERT_TRUE(syncChunk.chunkHighUSN());
+        previousChunkHighUsn = *syncChunk.chunkHighUSN();
+    }
+
+    const auto syncChunksFuture =
+        downloader.downloadSyncChunks(afterUsnInitial, ctx);
+
+    ASSERT_TRUE(syncChunksFuture.isFinished())
+        << testData.m_testName.toStdString();
+
+    ASSERT_EQ(syncChunksFuture.resultCount(), 1)
+        << testData.m_testName.toStdString();
+
     const auto syncChunksResult = syncChunksFuture.result();
-    ASSERT_EQ(syncChunksResult.m_exception, nullptr);
-    ASSERT_EQ(syncChunksResult.m_syncChunks.size(), 1);
-    EXPECT_EQ(syncChunksResult.m_syncChunks[0], syncChunk);
+    ASSERT_EQ(syncChunksResult.m_exception, nullptr)
+        << testData.m_testName.toStdString();
+
+    ASSERT_EQ(syncChunksResult.m_syncChunks, testData.m_syncChunks)
+        << testData.m_testName.toStdString();
 }
 
 } // namespace quentier::synchronization::tests
