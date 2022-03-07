79f5bc26aacc9ee2bcb807e6d0838aa0fc88f2b8
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2022-03-07 21:06:05 +0300

    Add mock for ISyncConflictResolver

---
+++ headers/quentier/synchronization/tests/mocks/MockISyncConflictResolver.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2022 Dmitry Ivanov
+ *
+ * This file is part of libquentier
+ *
+ * libquentier is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * libquentier is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libquentier. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <quentier/synchronization/ISyncConflictResolver.h>
+
+#include <gmock/gmock.h>
+
+namespace quentier::synchronization::tests::mocks {
+
+class MockISyncConflictResolver : public ISyncConflictResolver
+{
+public:
+    MOCK_METHOD(
+        QFuture<NotebookConflictResolution>, resolveNotebookConflict,
+        (qevercloud::Notebook theirs, qevercloud::Notebook mine), (override));
+
+    MOCK_METHOD(
+        QFuture<NoteConflictResolution>, resolveNoteConflict,
+        (qevercloud::Note theirs, qevercloud::Note mine), (override));
+
+    MOCK_METHOD(
+        QFuture<SavedSearchConflictResolution>, resolveSavedSearchConflict,
+        (qevercloud::SavedSearch theirs, qevercloud::SavedSearch mine),
+        (override));
+
+    MOCK_METHOD(
+        QFuture<TagConflictResolution>, resolveTagConflict,
+        (qevercloud::Tag theirs, qevercloud::Tag mine), (override));
+};
+
+} // namespace quentier::synchronization::tests::mocks
