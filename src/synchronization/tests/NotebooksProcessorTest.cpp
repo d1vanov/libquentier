a6e0e7aba1d4309a0da12585e264d5e6df4529e6
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2022-03-09 19:36:48 +0300

    Continue writing unit tests for NotebooksProcessor

--- src/synchronization/tests/NotebooksProcessorTest.cpp
+++ src/synchronization/tests/NotebooksProcessorTest.cpp
@@ -19,10 +19,22 @@
 #include <synchronization/processors/NotebooksProcessor.h>
 
 #include <quentier/exception/InvalidArgument.h>
+#include <quentier/exception/RuntimeError.h>
 #include <quentier/local_storage/tests/mocks/MockILocalStorage.h>
 #include <quentier/synchronization/tests/mocks/MockISyncConflictResolver.h>
+#include <quentier/threading/Future.h>
+#include <quentier/utility/UidGenerator.h>
+
+#include <qevercloud/types/builders/NotebookBuilder.h>
+#include <qevercloud/types/builders/SyncChunkBuilder.h>
+
+#include <QSet>
 
 #include <gtest/gtest.h>
+
+#include <algorithm>
+
+// clazy:excludeall=returning-void-expression
 
 namespace quentier::synchronization::tests {
 
@@ -61,6 +73,113 @@ TEST_F(NotebooksProcessorTest, CtorNullSyncConflictResolver)
         const auto notebooksProcessor = std::make_shared<NotebooksProcessor>(
             m_mockLocalStorage, nullptr),
         InvalidArgument);
+}
+
+TEST_F(NotebooksProcessorTest, ProcessNotebooksWithoutConflicts)
+{
+    const auto notebooks = QList<qevercloud::Notebook>{}
+        << qevercloud::NotebookBuilder{}
+               .setGuid(UidGenerator::Generate())
+               .setName(QStringLiteral("Notebook #1"))
+               .setUpdateSequenceNum(0)
+               .build()
+        << qevercloud::NotebookBuilder{}
+               .setGuid(UidGenerator::Generate())
+               .setName(QStringLiteral("Notebook #2"))
+               .setUpdateSequenceNum(35)
+               .build()
+        << qevercloud::NotebookBuilder{}
+               .setGuid(UidGenerator::Generate())
+               .setName(QStringLiteral("Notebook #3"))
+               .setUpdateSequenceNum(36)
+               .build()
+        << qevercloud::NotebookBuilder{}
+               .setGuid(UidGenerator::Generate())
+               .setName(QStringLiteral("Notebook #4"))
+               .setUpdateSequenceNum(54)
+               .build();
+
+    QList<qevercloud::Notebook> notebooksPutIntoLocalStorage;
+    QSet<qevercloud::Guid> triedGuids;
+    QSet<QString> triedNames;
+
+    EXPECT_CALL(*m_mockLocalStorage, findNotebookByGuid)
+        .WillRepeatedly([&](const qevercloud::Guid & guid) {
+            EXPECT_FALSE(triedGuids.contains(guid));
+            triedGuids.insert(guid);
+
+            const auto it = std::find_if(
+                notebooksPutIntoLocalStorage.constBegin(),
+                notebooksPutIntoLocalStorage.constEnd(),
+                [&](const qevercloud::Notebook & notebook)
+                {
+                    return notebook.guid() && (*notebook.guid() == guid);
+                });
+            if (it != notebooksPutIntoLocalStorage.constEnd()) {
+                return threading::makeReadyFuture<
+                    std::optional<qevercloud::Notebook>>(*it);
+            }
+
+            return threading::makeReadyFuture<
+                std::optional<qevercloud::Notebook>>(std::nullopt);
+        });
+
+    EXPECT_CALL(*m_mockLocalStorage, findNotebookByName)
+        .WillRepeatedly([&](const QString & name,
+                            const std::optional<QString> & linkedNotebookGuid) {
+            EXPECT_FALSE(triedNames.contains(name));
+            triedNames.insert(name);
+
+            EXPECT_FALSE(linkedNotebookGuid);
+
+            const auto it = std::find_if(
+                notebooksPutIntoLocalStorage.constBegin(),
+                notebooksPutIntoLocalStorage.constEnd(),
+                [&](const qevercloud::Notebook & notebook) {
+                    return notebook.name() && (*notebook.name() == name);
+                });
+            if (it != notebooksPutIntoLocalStorage.constEnd()) {
+                return threading::makeReadyFuture<
+                    std::optional<qevercloud::Notebook>>(*it);
+            }
+
+            return threading::makeReadyFuture<
+                std::optional<qevercloud::Notebook>>(std::nullopt);
+        });
+
+    EXPECT_CALL(*m_mockLocalStorage, putNotebook)
+        .WillRepeatedly(
+            [&](const qevercloud::Notebook & notebook)
+            {
+                if (Q_UNLIKELY(!notebook.guid())) {
+                    return threading::makeExceptionalFuture<void>(RuntimeError{
+                        ErrorString{"Detected notebook without guid"}});
+                }
+
+                EXPECT_TRUE(triedGuids.contains(*notebook.guid()));
+
+                if (Q_UNLIKELY(!notebook.name())) {
+                    return threading::makeExceptionalFuture<void>(RuntimeError{
+                        ErrorString{"Detected notebook without name"}});
+                }
+
+                EXPECT_TRUE(triedNames.contains(*notebook.name()));
+
+                notebooksPutIntoLocalStorage << notebook;
+                return threading::makeReadyFuture();
+            });
+
+    const auto syncChunks = QList<qevercloud::SyncChunk>{}
+        << qevercloud::SyncChunkBuilder{}.setNotebooks(notebooks).build();
+
+    const auto notebooksProcessor = std::make_shared<NotebooksProcessor>(
+        m_mockLocalStorage, m_mockSyncConflictResolver);
+
+    auto future = notebooksProcessor->processNotebooks(syncChunks);
+    ASSERT_TRUE(future.isFinished());
+    EXPECT_NO_THROW(future.waitForFinished());
+
+    EXPECT_EQ(notebooksPutIntoLocalStorage, notebooks);
 }
 
 } // namespace quentier::synchronization::tests
