d94d4152a70c8556808b813805cbd415e5b1ef39
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2022-03-21 18:50:26 +0300

    Add TrackedTask template class to simplify handling of selfWeak in code of future continuations

---
+++ headers/quentier/threading/TrackedTask.h
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2022 Dmitry Ivanov
+ *
+ * This file is part of libquentier
+ *
+ * libquentier is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * libquentier is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libquentier. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <functional>
+#include <type_traits>
+#include <utility>
+
+namespace quentier::threading {
+
+namespace detail {
+
+template <typename LockableObject, typename Function, typename... Arguments>
+constexpr std::enable_if_t<std::is_invocable_v<Function, Arguments...>> invoke(
+    LockableObject & lockableObject, Function & function,
+    Arguments &&... arguments)
+{
+    const auto lockedObject = lockableObject.lock();
+    if (lockedObject) {
+        std::invoke(function, std::forward<Arguments>(arguments)...);
+    }
+}
+
+template <typename LockableObject, typename Function, typename... Arguments>
+constexpr std::enable_if_t<!std::is_invocable_v<Function, Arguments...>> invoke(
+    LockableObject & lockableObject, Function & function,
+    Arguments &&... arguments)
+{
+    const auto lockedObject = lockableObject.lock();
+    if (lockedObject) {
+        std::invoke(
+            function, *lockedObject, std::forward<Arguments>(arguments)...);
+    }
+}
+
+} // namespace detail
+
+/**
+ * Wrapper class which automates checking for the state of a lockable object.
+ * With this class code like this
+ *
+ * auto task = [selfWeak = weak_from_this()] {
+ *     auto self = selfWeak.lock();
+ *     if (!self) {
+ *         return;
+ *     }
+ *     // otherwise do something
+ * };
+ *
+ * can be written like this:
+ *
+ * auto task = threading::TrackedTask{weak_from_this(), &MyClass::someMethod};
+ */
+template <typename LockableObject, typename Function>
+class TrackedTask
+{
+public:
+    template <typename SomeLockableObject, typename SomeFunction>
+    constexpr TrackedTask(
+        SomeLockableObject && someLockableObject,
+        SomeFunction && function) :
+        m_lockableObject{std::forward<SomeLockableObject>(someLockableObject)},
+        m_function{std::forward<SomeFunction>(function)}
+    {}
+
+	template<typename... Arguments>
+	constexpr void operator()(Arguments &&... arguments)
+	{
+		detail::invoke(
+            m_lockableObject, m_function,
+            std::forward<Arguments>(arguments)...);
+	}
+
+	template<typename... Arguments>
+	constexpr void operator()(Arguments &&... arguments) const
+	{
+        detail::invoke(
+            m_lockableObject, m_function,
+            std::forward<Arguments>(arguments)...);
+    }
+
+private:
+    LockableObject m_lockableObject;
+    Function m_function;
+};
+
+template <typename LockableObject, typename Function>
+TrackedTask(LockableObject, Function)
+    -> TrackedTask<LockableObject, Function>;
+
+} // namespace quentier::threading
---
+++ src/threading/tests/TrackedTaskTest.cpp
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2022 Dmitry Ivanov
+ *
+ * This file is part of libquentier
+ *
+ * libquentier is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * libquentier is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libquentier. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <quentier/threading/TrackedTask.h>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <memory>
+
+// clazy:excludeall=returning-void-expression
+
+namespace quentier::threading::tests {
+
+using testing::Invoke;
+using testing::StrictMock;
+
+struct MockCallable
+{
+    MOCK_METHOD(void, call0, ());
+    MOCK_METHOD(void, call2, (bool, int));
+};
+
+TEST(TrackedTaskTest, ExpiredObject)
+{
+    std::weak_ptr<void> selfWeak;
+    const TrackedTask trackedTask{selfWeak, [] { ADD_FAILURE(); }};
+
+    trackedTask();
+}
+
+TEST(TrackedTaskTest, NonExpiredObject)
+{
+    const auto object = std::make_shared<int>(1);
+    const std::weak_ptr<void> selfWeak = object;
+
+    StrictMock<MockCallable> mockCallable;
+    EXPECT_CALL(mockCallable, call0()).Times(1);
+
+    const TrackedTask trackedTask{
+        selfWeak, Invoke(&mockCallable, &MockCallable::call0)};
+
+    trackedTask();
+}
+
+TEST(TrackedTaskTest, CustomLockableObject)
+{
+    struct CustomLockable
+    {
+        [[nodiscard]] bool lock() const noexcept
+        {
+            return true;
+        }
+    };
+
+    StrictMock<MockCallable> mockCallable;
+    EXPECT_CALL(mockCallable, call0()).Times(1);
+
+    const TrackedTask trackedTask{
+        CustomLockable{}, Invoke(&mockCallable, &MockCallable::call0)};
+
+    trackedTask();
+}
+
+TEST(TrackedTaskTest, PassArguments)
+{
+    const auto object = std::make_shared<int>(1);
+    const std::weak_ptr<void> selfWeak = object;
+
+    StrictMock<MockCallable> mockCallable;
+    EXPECT_CALL(mockCallable, call2(false, 1)).Times(1);
+
+    const TrackedTask callback{
+        selfWeak, Invoke(&mockCallable, &MockCallable::call2)};
+
+    callback(false, 1);
+}
+
+TEST(TrackedTaskTest, CallMember)
+{
+    const auto object = std::make_shared<MockCallable>();
+    const std::weak_ptr<MockCallable> selfWeak = object;
+
+    EXPECT_CALL(*object, call0()).Times(1);
+
+    const TrackedTask trackedTask{selfWeak, &MockCallable::call0};
+
+    trackedTask();
+}
+
+TEST(TrackedTaskTest, CallLink)
+{
+    const auto object = std::make_shared<MockCallable>();
+    const std::weak_ptr<MockCallable> selfWeak = object;
+
+    EXPECT_CALL(*object, call0()).Times(1);
+
+    const TrackedTask trackedTask{
+        selfWeak, [](MockCallable & mockCallable) { mockCallable.call0(); }};
+
+    trackedTask();
+}
+
+TEST(TrackedTaskTest, PassArgumentsPack)
+{
+	const auto object = std::make_shared<MockCallable>();
+	const std::weak_ptr<MockCallable> selfWeak = object;
+
+	EXPECT_CALL(*object, call0()).Times(1);
+
+    const TrackedTask trackedTask{
+        selfWeak, [&object](auto &&... arguments) {
+            object->call0(std::forward<decltype(arguments)>(arguments)...);
+        }};
+
+    trackedTask();
+}
+
+} // namespace quentier::threading::tests
