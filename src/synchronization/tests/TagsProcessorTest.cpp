717d267f7d2d0be285ac5706ef6b87cf661b0c27
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2022-03-24 18:06:33 +0300

    Add a couple more unit tests for TagsProcessor

--- src/synchronization/tests/TagsProcessorTest.cpp
+++ src/synchronization/tests/TagsProcessorTest.cpp
@@ -24,6 +24,7 @@
 #include <quentier/local_storage/tests/mocks/MockILocalStorage.h>
 #include <quentier/synchronization/tests/mocks/MockISyncConflictResolver.h>
 #include <quentier/threading/Future.h>
+#include <quentier/utility/TagSortByParentChildRelations.h>
 #include <quentier/utility/UidGenerator.h>
 
 #include <qevercloud/types/builders/SyncChunkBuilder.h>
@@ -88,6 +89,214 @@ TEST_F(TagsProcessorTest, CtorNullSyncChunksDataCounters)
         const auto tagsProcessor = std::make_shared<TagsProcessor>(
             m_mockLocalStorage, m_mockSyncConflictResolver, nullptr),
         InvalidArgument);
+}
+
+TEST_F(TagsProcessorTest, ProcessSyncChunksWithoutTagsToProcess)
+{
+    const auto syncChunks = QList<qevercloud::SyncChunk>{}
+        << qevercloud::SyncChunkBuilder{}.build();
+
+    const auto tagsProcessor = std::make_shared<TagsProcessor>(
+        m_mockLocalStorage, m_mockSyncConflictResolver,
+        m_syncChunksDataCounters);
+
+    auto future = tagsProcessor->processTags(syncChunks);
+    ASSERT_TRUE(future.isFinished());
+    EXPECT_NO_THROW(future.waitForFinished());
+
+    EXPECT_EQ(m_syncChunksDataCounters->totalTags(), 0UL);
+    EXPECT_EQ(m_syncChunksDataCounters->totalExpungedTags(), 0UL);
+    EXPECT_EQ(m_syncChunksDataCounters->addedTags(), 0UL);
+    EXPECT_EQ(m_syncChunksDataCounters->updatedTags(), 0UL);
+    EXPECT_EQ(m_syncChunksDataCounters->expungedTags(), 0UL);
+}
+
+TEST_F(TagsProcessorTest, ProcessTagsWithoutConflicts)
+{
+    // Put tags into such an order that child tags come before parent ones,
+    // to ensure that TagsProcessor would properly sort them and put parent ones
+    // into the local storage first
+    const auto tag4 = qevercloud::TagBuilder{}
+                          .setGuid(UidGenerator::Generate())
+                          .setName(QStringLiteral("Tag #4"))
+                          .setUpdateSequenceNum(36)
+                          .build();
+
+    const auto tag1 = qevercloud::TagBuilder{}
+                          .setGuid(UidGenerator::Generate())
+                          .setName(QStringLiteral("Tag #1"))
+                          .setUpdateSequenceNum(32)
+                          .setParentGuid(tag4.guid())
+                          .build();
+
+    const auto tag3 = qevercloud::TagBuilder{}
+                          .setGuid(UidGenerator::Generate())
+                          .setName(QStringLiteral("Tag #3"))
+                          .setUpdateSequenceNum(35)
+                          .build();
+
+    const auto tag2 = qevercloud::TagBuilder{}
+                          .setGuid(UidGenerator::Generate())
+                          .setName(QStringLiteral("Tag #2"))
+                          .setUpdateSequenceNum(33)
+                          .build();
+
+    const auto tags = QList<qevercloud::Tag>{} << tag1 << tag2 << tag3 << tag4;
+
+    QList<qevercloud::Tag> tagsPutIntoLocalStorage;
+    QSet<qevercloud::Guid> triedGuids;
+    QSet<QString> triedNames;
+
+    EXPECT_CALL(*m_mockLocalStorage, findTagByGuid)
+        .WillRepeatedly([&](const qevercloud::Guid & guid) {
+            EXPECT_FALSE(triedGuids.contains(guid));
+            triedGuids.insert(guid);
+
+            const auto it = std::find_if(
+                tagsPutIntoLocalStorage.constBegin(),
+                tagsPutIntoLocalStorage.constEnd(),
+                [&](const qevercloud::Tag & tag) {
+                    return tag.guid() && (*tag.guid() == guid);
+                });
+            if (it != tagsPutIntoLocalStorage.constEnd()) {
+                return threading::makeReadyFuture<
+                    std::optional<qevercloud::Tag>>(*it);
+            }
+
+            return threading::makeReadyFuture<std::optional<qevercloud::Tag>>(
+                std::nullopt);
+        });
+
+    EXPECT_CALL(*m_mockLocalStorage, findTagByName)
+        .WillRepeatedly([&](const QString & name,
+                            const std::optional<QString> & linkedNotebookGuid) {
+            EXPECT_FALSE(triedNames.contains(name));
+            triedNames.insert(name);
+
+            EXPECT_FALSE(linkedNotebookGuid);
+
+            const auto it = std::find_if(
+                tagsPutIntoLocalStorage.constBegin(),
+                tagsPutIntoLocalStorage.constEnd(),
+                [&](const qevercloud::Tag & tag) {
+                    return tag.name() && (*tag.name() == name);
+                });
+            if (it != tagsPutIntoLocalStorage.constEnd()) {
+                return threading::makeReadyFuture<
+                    std::optional<qevercloud::Tag>>(*it);
+            }
+
+            return threading::makeReadyFuture<std::optional<qevercloud::Tag>>(
+                std::nullopt);
+        });
+
+    EXPECT_CALL(*m_mockLocalStorage, putTag)
+        .WillRepeatedly([&](const qevercloud::Tag & tag) {
+            if (Q_UNLIKELY(!tag.guid())) {
+                return threading::makeExceptionalFuture<void>(
+                    RuntimeError{ErrorString{"Detected tag without guid"}});
+            }
+
+            EXPECT_TRUE(triedGuids.contains(*tag.guid()));
+
+            if (Q_UNLIKELY(!tag.name())) {
+                return threading::makeExceptionalFuture<void>(
+                    RuntimeError{ErrorString{"Detected tag without name"}});
+            }
+
+            EXPECT_TRUE(triedNames.contains(*tag.name()));
+
+            if (tag.parentGuid()) {
+                const auto it = std::find_if(
+                    tagsPutIntoLocalStorage.constBegin(),
+                    tagsPutIntoLocalStorage.constEnd(),
+                    [parentGuid =
+                         *tag.parentGuid()](const qevercloud::Tag & tag) {
+                        return tag.guid() && (*tag.guid() == parentGuid);
+                    });
+                if (it == tagsPutIntoLocalStorage.constEnd()) {
+                    return threading::makeExceptionalFuture<
+                        void>(RuntimeError{ErrorString{
+                        "Detected attempt to put child tag before parent"}});
+                }
+            }
+
+            tagsPutIntoLocalStorage << tag;
+            return threading::makeReadyFuture();
+        });
+
+    const auto syncChunks = QList<qevercloud::SyncChunk>{}
+        << qevercloud::SyncChunkBuilder{}.setTags(tags).build();
+
+    const auto tagsProcessor = std::make_shared<TagsProcessor>(
+        m_mockLocalStorage, m_mockSyncConflictResolver,
+        m_syncChunksDataCounters);
+
+    auto future = tagsProcessor->processTags(syncChunks);
+    ASSERT_TRUE(future.isFinished());
+    EXPECT_NO_THROW(future.waitForFinished());
+
+    auto sortedTags = tags;
+    ErrorString error;
+    ASSERT_TRUE(sortTagsByParentChildRelations(sortedTags, error))
+        << error.nonLocalizedString().toStdString();
+
+    EXPECT_EQ(tagsPutIntoLocalStorage, sortedTags);
+
+    EXPECT_EQ(
+        m_syncChunksDataCounters->totalTags(),
+        static_cast<quint64>(tags.size()));
+
+    EXPECT_EQ(m_syncChunksDataCounters->totalExpungedTags(), 0UL);
+
+    EXPECT_EQ(
+        m_syncChunksDataCounters->addedTags(),
+        static_cast<quint64>(tags.size()));
+
+    EXPECT_EQ(m_syncChunksDataCounters->updatedTags(), 0UL);
+    EXPECT_EQ(m_syncChunksDataCounters->expungedTags(), 0UL);
+}
+
+TEST_F(TagsProcessorTest, ProcessExpungedTags)
+{
+    const auto expungedTagGuids = QList<qevercloud::Guid>{}
+        << UidGenerator::Generate() << UidGenerator::Generate()
+        << UidGenerator::Generate();
+
+    const auto syncChunks = QList<qevercloud::SyncChunk>{}
+        << qevercloud::SyncChunkBuilder{}
+               .setExpungedTags(expungedTagGuids)
+               .build();
+
+    const auto tagsProcessor = std::make_shared<TagsProcessor>(
+        m_mockLocalStorage, m_mockSyncConflictResolver,
+        m_syncChunksDataCounters);
+
+    QList<qevercloud::Guid> processedTagGuids;
+    EXPECT_CALL(*m_mockLocalStorage, expungeTagByGuid)
+        .WillRepeatedly([&](const qevercloud::Guid & tagGuid) {
+            processedTagGuids << tagGuid;
+            return threading::makeReadyFuture();
+        });
+
+    auto future = tagsProcessor->processTags(syncChunks);
+    ASSERT_TRUE(future.isFinished());
+    EXPECT_NO_THROW(future.waitForFinished());
+
+    EXPECT_EQ(processedTagGuids, expungedTagGuids);
+
+    EXPECT_EQ(m_syncChunksDataCounters->totalTags(), 0UL);
+
+    EXPECT_EQ(
+        m_syncChunksDataCounters->totalExpungedTags(),
+        static_cast<quint64>(expungedTagGuids.size()));
+
+    EXPECT_EQ(m_syncChunksDataCounters->addedTags(), 0UL);
+    EXPECT_EQ(m_syncChunksDataCounters->updatedTags(), 0UL);
+
+    EXPECT_EQ(
+        m_syncChunksDataCounters->expungedTags(),
+        static_cast<quint64>(expungedTagGuids.size()));
 }
 
 } // namespace quentier::synchronization::tests
