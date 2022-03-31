604fa226453a97b3f5401aaf74ce8de38f1d70b0
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2022-03-31 19:04:08 +0300

    Continue implementing NotesProcessor

--- src/synchronization/processors/NotesProcessor.cpp
+++ src/synchronization/processors/NotesProcessor.cpp
@@ -33,9 +33,11 @@
 #include <QFutureWatcher>
 #include <QMutex>
 #include <QMutexLocker>
+#include <QPointer>
 
 #include <algorithm>
 #include <cmath>
+#include <type_traits>
 
 namespace quentier::synchronization {
 
@@ -83,6 +85,156 @@ namespace {
     const qevercloud::SyncChunk & syncChunk)
 {
     return syncChunk.expungedNotes().value_or(QList<qevercloud::Guid>{});
+}
+
+// Maps progress values from two futures into the progress for the separate
+// progress
+template <class T, class U, class V>
+void mapProgress(
+    QFuture<T> firstFuture, QFuture<U> secondFuture,
+    std::shared_ptr<QPromise<V>> promise)
+{
+    Q_ASSERT(firstFuture.progressMaximum() >= 0);
+    Q_ASSERT(firstFuture.progressMinimum() >= 0);
+    Q_ASSERT(firstFuture.progressMaximum() >= firstFuture.progressMinimum());
+
+    Q_ASSERT(secondFuture.progressMaximum() >= 0);
+    Q_ASSERT(secondFuture.progressMinimum() >= 0);
+    Q_ASSERT(secondFuture.progressMaximum() >= secondFuture.progressMinimum());
+
+    const int firstFutureProgressRange =
+        firstFuture.progressMaximum() - firstFuture.progressMinimum();
+
+    const int secondFutureProgressRange =
+        secondFuture.progressMaximum() - secondFuture.progressMinimum();
+
+    promise->setProgressRange(0, 100);
+    promise->setProgressValue(0);
+
+    auto promiseProgressMutex = std::make_shared<QMutex>();
+
+    const auto computePromiseProgress = [promiseProgressMutex](
+        const int currentFutureProgressRange,
+        const int currentFutureProgress, // NOLINT
+        const int otherFutureProgressRange,
+        const std::shared_ptr<double> & currentFutureProgressPercentage,
+        const std::shared_ptr<double> & otherFutureProgressPercentage)
+    {
+        Q_ASSERT(currentFutureProgressRange);
+        Q_ASSERT(otherFutureProgressRange);
+
+        // Convert current future progress into a percentage
+        *currentFutureProgressPercentage = [&] {
+            if (currentFutureProgressRange == 0) {
+                return 0.0;
+            }
+
+            return std::clamp(
+                static_cast<double>(currentFutureProgress) /
+                    currentFutureProgressRange,
+                0.0, 1.0);
+        }();
+
+        const QMutexLocker lock{promiseProgressMutex.get()};
+
+        const double newProgress = [&] {
+            if (currentFutureProgressRange == 0 &&
+                otherFutureProgressRange == 0) {
+                return 0.0;
+            }
+
+            return (*currentFutureProgressPercentage *
+                        currentFutureProgressRange +
+                    *otherFutureProgressPercentage * otherFutureProgressRange) /
+                (currentFutureProgressRange + otherFutureProgressRange);
+        }();
+
+        return std::clamp(
+            static_cast<int>(std::round(newProgress * 100.0)), 0, 100);
+    };
+
+    auto firstFutureProgressPercentage = std::make_shared<double>(0.0);
+    auto secondFutureProgressPercentage = std::make_shared<double>(0.0);
+
+    auto firstFutureWatcher = std::make_unique<QFutureWatcher<T>>();
+    firstFutureWatcher->setFuture(firstFuture);
+
+    QObject::connect(
+        firstFutureWatcher.get(), &QFutureWatcher<T>::progressValueChanged,
+        firstFutureWatcher.get(),
+        [firstFutureProgressPercentage, secondFutureProgressPercentage,
+         firstFutureProgressRange, secondFutureProgressRange, promise,
+         promiseProgressMutex, computePromiseProgress](int progressValue) {
+            const int newProgress = computePromiseProgress(
+                firstFutureProgressRange, progressValue,
+                secondFutureProgressRange, firstFutureProgressPercentage,
+                secondFutureProgressPercentage);
+
+            promise->setProgressValue(
+                std::max(promise->future().progressValue(), newProgress));
+        });
+
+    QObject::connect(
+        firstFutureWatcher.get(), &QFutureWatcher<T>::finished,
+        firstFutureWatcher.get(),
+        [firstFutureWatcher =
+             QPointer<QFutureWatcher<T>>(firstFutureWatcher.get())] {
+            if (!firstFutureWatcher.isNull()) {
+                firstFutureWatcher->deleteLater();
+            }
+        });
+
+    QObject::connect(
+        firstFutureWatcher.get(), &QFutureWatcher<T>::canceled,
+        firstFutureWatcher.get(),
+        [firstFutureWatcher =
+             QPointer<QFutureWatcher<T>>(firstFutureWatcher.get())] {
+            if (!firstFutureWatcher.isNull()) {
+                firstFutureWatcher->deleteLater();
+            }
+        });
+
+    Q_UNUSED(firstFutureWatcher.release())
+
+    auto secondFutureWatcher = std::make_unique<QFutureWatcher<U>>();
+    secondFutureWatcher->setFuture(secondFuture);
+
+    QObject::connect(
+        secondFutureWatcher.get(), &QFutureWatcher<U>::progressValueChanged,
+        secondFutureWatcher.get(),
+        [firstFutureProgressPercentage, secondFutureProgressPercentage,
+         firstFutureProgressRange, secondFutureProgressRange, promise,
+         promiseProgressMutex, computePromiseProgress](int progressValue) {
+            const int newProgress = computePromiseProgress(
+                secondFutureProgressRange, progressValue,
+                firstFutureProgressRange, secondFutureProgressPercentage,
+                firstFutureProgressPercentage);
+
+            promise->setProgressValue(
+                std::max(promise->future().progressValue(), newProgress));
+         });
+
+    QObject::connect(
+        secondFutureWatcher.get(), &QFutureWatcher<U>::finished,
+        secondFutureWatcher.get(),
+        [secondFutureWatcher =
+             QPointer<QFutureWatcher<U>>(secondFutureWatcher.get())] {
+            if (!secondFutureWatcher.isNull()) {
+                secondFutureWatcher->deleteLater();
+            }
+        });
+
+    QObject::connect(
+        secondFutureWatcher.get(), &QFutureWatcher<U>::canceled,
+        secondFutureWatcher.get(),
+        [secondFutureWatcher =
+             QPointer<QFutureWatcher<U>>(secondFutureWatcher.get())] {
+            if (!secondFutureWatcher.isNull()) {
+                secondFutureWatcher->deleteLater();
+            }
+        });
+
+    Q_UNUSED(secondFutureWatcher.release());
 }
 
 } // namespace
@@ -199,125 +351,8 @@ QFuture<INotesProcessor::ProcessNotesStatus> NotesProcessor::processNotes(
     auto future = promise->future();
     promise->start();
 
-    promise->setProgressRange(0, 100);
-    promise->setProgressValue(0);
+    mapProgress(processNotesFuture, expungeNotesFuture, promise);
 
-    auto promiseProgressMutex = std::make_shared<QMutex>();
-
-    // Need to map progress values from two different futures of different types
-    // into the single promise
-
-    // TODO: refactor this whole stuff - move to a separate method or something
-
-    auto processNotesFutureProgress = std::make_shared<double>(0.0);
-    auto expungeNotesFutureProgress = std::make_shared<double>(0.0);
-
-    // Start with the future representing the processing of new/updated notes
-    const int maxProcessNotesFutureProgress =
-        processNotesFuture.progressMaximum();
-
-    const int minProcessNotesFutureProgress =
-        processNotesFuture.progressMinimum();
-
-    Q_ASSERT(maxProcessNotesFutureProgress >= 0);
-    Q_ASSERT(minProcessNotesFutureProgress >= 0);
-    Q_ASSERT(maxProcessNotesFutureProgress >= minProcessNotesFutureProgress);
-
-    const int processNotesFutureProgressRange =
-        maxProcessNotesFutureProgress - minProcessNotesFutureProgress;
-
-    auto processNotesFutureWatcher =
-        threading::makeFutureWatcher<QList<ProcessNoteStatus>>();
-
-    processNotesFutureWatcher->setFuture(processNotesFuture);
-    auto * rawProcessNotesFutureWatcher = processNotesFutureWatcher.get();
-
-    QObject::connect(
-        rawProcessNotesFutureWatcher,
-        &QFutureWatcher<QList<ProcessNoteStatus>>::progressValueChanged,
-        rawProcessNotesFutureWatcher,
-        [totalItemCount, noteCount, expungedNoteCount,
-         processNotesFutureProgressRange, promise, promiseProgressMutex,
-         processNotesFutureProgress, expungeNotesFutureProgress](
-             int progressValue) {
-             // Convert progressValue into percentage
-             *processNotesFutureProgress = [&] {
-                 if (processNotesFutureProgressRange == 0) {
-                     return 0.0;
-                 }
-
-                 return std::clamp(
-                     static_cast<double>(progressValue) /
-                         processNotesFutureProgressRange,
-                     0.0, 1.0);
-             }();
-
-             const QMutexLocker lock{promiseProgressMutex.get()};
-
-             const auto newProgress = *processNotesFutureProgress * noteCount /
-                     static_cast<double>(totalItemCount) +
-                 *expungeNotesFutureProgress * expungedNoteCount /
-                     static_cast<double>(totalItemCount);
-
-             promise->setProgressValue(std::max(
-                 promise->future().progressValue(),
-                 std::clamp(
-                     static_cast<int>(std::round(newProgress * 100.0)), 0,
-                     100)));
-         });
-
-    // Now handle the future representing the expunging of expunged notes
-    const int maxExpungeNotesFutureProgress =
-        expungeNotesFuture.progressMaximum();
-
-    const int minExpungeNotesFutureProgress =
-        expungeNotesFuture.progressMinimum();
-
-    Q_ASSERT(maxExpungeNotesFutureProgress >= 0);
-    Q_ASSERT(minExpungeNotesFutureProgress >= 0);
-    Q_ASSERT(maxExpungeNotesFutureProgress >= minExpungeNotesFutureProgress);
-
-    const int expungeNotesFutureProgressRange =
-        maxExpungeNotesFutureProgress - minExpungeNotesFutureProgress;
-
-    auto expungeNotesFutureWatcher = threading::makeFutureWatcher<void>();
-    expungeNotesFutureWatcher->setFuture(expungeNotesFuture);
-    auto * rawExpungeNotesFutureWatcher = expungeNotesFutureWatcher.get();
-
-    QObject::connect(
-        rawExpungeNotesFutureWatcher,
-        &QFutureWatcher<void>::progressValueChanged,
-        rawExpungeNotesFutureWatcher,
-        [totalItemCount, noteCount, expungedNoteCount,
-         expungeNotesFutureProgressRange, promise, promiseProgressMutex,
-         processNotesFutureProgress, expungeNotesFutureProgress](int progressValue) {
-             // Convert progressValue into percentage
-             *expungeNotesFutureProgress = [&] {
-                 if (expungeNotesFutureProgressRange == 0) {
-                     return 0.0;
-                 }
-
-                 return std::clamp(
-                     static_cast<double>(progressValue) /
-                         expungeNotesFutureProgressRange,
-                     0.0, 1.0);
-             }();
-
-             const QMutexLocker lock{promiseProgressMutex.get()};
-
-             const auto newProgress = *processNotesFutureProgress * noteCount /
-                     static_cast<double>(totalItemCount) +
-                 *expungeNotesFutureProgress * expungedNoteCount /
-                     static_cast<double>(totalItemCount);
-
-             promise->setProgressValue(std::max(
-                 promise->future().progressValue(),
-                 std::clamp(
-                     static_cast<int>(std::round(newProgress * 100.0)), 0,
-                     100)));
-         });
-
-    // TODO: watch the progress of expungedNotes similarly
     // TODO: set up handling of finalization and errors of both futures
 
     return future;
