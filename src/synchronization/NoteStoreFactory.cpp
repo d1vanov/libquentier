9f133758093c7539f058d0b1a271d2ca32ab7f0c
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2022-08-09 18:44:20 +0300

    Add simplest possible NoteStoreFactory implementation

---
+++ src/synchronization/NoteStoreFactory.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2022 Dmitry Ivanov
+ *
+ * This file is part of libquentier
+ *
+ * libquentier is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * libquentier is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libquentier. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "NoteStoreFactory.h"
+
+#include <qevercloud/services/INoteStore.h>
+
+namespace quentier::synchronization {
+
+qevercloud::INoteStorePtr NoteStoreFactory::noteStore(
+    QString noteStoreUrl,
+    std::optional<qevercloud::Guid> linkedNotebookGuid,
+    qevercloud::IRequestContextPtr ctx,
+    qevercloud::IRetryPolicyPtr retryPolicy)
+{
+    return qevercloud::newNoteStore(
+        std::move(noteStoreUrl), std::move(linkedNotebookGuid), std::move(ctx),
+        std::move(retryPolicy));
+}
+
+} // namespace quentier::synchronization
