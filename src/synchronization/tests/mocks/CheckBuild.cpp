cb92921316853875838193b599db301a7dbc6f17
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2022-08-23 18:45:03 +0300

    Add mock for IAccountLimitsProvider

--- src/synchronization/tests/mocks/CheckBuild.cpp
+++ src/synchronization/tests/mocks/CheckBuild.cpp
@@ -1,6 +1,7 @@
 #include <quentier/synchronization/tests/mocks/MockIAuthenticator.h>
 #include <quentier/synchronization/tests/mocks/MockISyncConflictResolver.h>
 #include <quentier/synchronization/tests/mocks/MockISyncStateStorage.h>
+#include "MockIAccountLimitsProvider.h"
 #include "MockIAuthenticationInfoProvider.h"
 #include "MockIDurableNotesProcessor.h"
 #include "MockIDurableResourcesProcessor.h"
