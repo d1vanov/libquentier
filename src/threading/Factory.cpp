9bee319dd055d5b53ebfafe78a30a75a52d92630
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2023-01-15 11:23:02 +0300

    Add factory function returning shared_ptr operating QThreadPool::globalInstance

---
+++ src/threading/Factory.cpp
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2023 Dmitry Ivanov
+ *
+ * This file is part of libquentier
+ *
+ * libquentier is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * libquentier is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libquentier. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <quentier/threading/Factory.h>
+
+#include <QThreadPool>
+
+namespace quentier::threading {
+
+QThreadPoolPtr globalThreadPool()
+{
+    return {
+        QThreadPool::globalInstance(),
+        []([[maybe_unused]] const QThreadPool * threadPool) {
+            // do nothing
+        }};
+}
+
+} // namespace quentier::threading
