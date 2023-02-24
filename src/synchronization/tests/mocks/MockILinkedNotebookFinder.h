08e708877d328960912295cc23c8acae7d5a57d9
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2023-02-24 19:04:53 +0300

    Add mock for ILinkedNotebookFinder

---
+++ src/synchronization/tests/mocks/MockILinkedNotebookFinder.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2023 Dmitry Ivanov
+ *
+ * This file is part of libquentier
+ *
+ * libquentier is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * libquentier is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libquentier. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <synchronization/ILinkedNotebookFinder.h>
+
+#include <gmock/gmock.h>
+
+namespace quentier::synchronization::tests::mocks {
+
+class MockILinkedNotebookFinder : public ILinkedNotebookFinder
+{
+public:
+    MOCK_METHOD(
+        QFuture<std::optional<qevercloud::LinkedNotebook>>,
+        findLinkedNotebookByNotebookLocalId,
+        (const QString & notebookLocalId), (override));
+
+    MOCK_METHOD(
+        QFuture<std::optional<qevercloud::LinkedNotebook>>,
+        findLinkedNotebookByGuid,
+        (const qevercloud::Guid & guid), (override));
+};
+
+} // namespace quentier::synchronization::tests::mocks
