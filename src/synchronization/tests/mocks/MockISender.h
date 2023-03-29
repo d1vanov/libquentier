421b8699b29a4c5110db6b566ce45fb3a3421ddc
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2023-03-29 20:18:36 +0300

    Add mock for ISender

---
+++ src/synchronization/tests/mocks/MockISender.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2023 Dmitry Ivanov
+ *
+ * This file is part of libquentier
+ *
+ * libquentier is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * libquentier is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libquentier. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <synchronization/ISender.h>
+
+#include <gmock/gmock.h>
+
+// clazy:excludeall=returning-void-expression
+
+namespace quentier::synchronization::tests::mocks {
+
+class MockISender : public ISender
+{
+public:
+    MOCK_METHOD(
+        QFuture<ISender::Result>, send,
+        (utility::cancelers::ICancelerPtr canceler,
+         ISender::ICallbackWeakPtr callbackWeak),
+        (override));
+};
+
+} // namespace quentier::synchronization::tests::mocks
