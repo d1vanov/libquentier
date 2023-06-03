6b54e10cb62a4716456a87fc713d4fca90c591cf
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2023-06-03 11:39:58 +0300

    Complete the set of unit tests for Synchronizer

--- src/synchronization/tests/SynchronizerTest.cpp
+++ src/synchronization/tests/SynchronizerTest.cpp
@@ -23,20 +23,32 @@
 #include <synchronization/tests/mocks/MockIAuthenticationInfoProvider.h>
 #include <synchronization/types/AuthenticationInfo.h>
 #include <synchronization/types/SyncOptionsBuilder.h>
+#include <synchronization/types/SyncResult.h>
 
 #include <quentier/exception/InvalidArgument.h>
 #include <quentier/local_storage/tests/mocks/MockILocalStorage.h>
+#include <quentier/synchronization/ISyncEventsNotifier.h>
 #include <quentier/synchronization/tests/mocks/MockISyncConflictResolver.h>
 #include <quentier/threading/Future.h>
 #include <quentier/utility/cancelers/ManualCanceler.h>
+
+#include <QCoreApplication>
+
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+#include <QPromise>
+#else
+#include <quentier/threading/Qt5Promise.h>
+#endif
 
 #include <gtest/gtest.h>
 
 // clazy:excludeall=non-pod-global-static
 // clazy:excludeall=returning-void-expression
+// clazy:excludeall=lambda-in-connect
 
 namespace quentier::synchronization::tests {
 
+using testing::_;
 using testing::Return;
 using testing::StrictMock;
 
@@ -52,8 +64,8 @@ protected:
         QStringLiteral("shard id")};
 
     const std::shared_ptr<mocks::MockIAccountSynchronizer>
-        m_mockAccountSynchronizer = std::make_shared<
-            StrictMock<mocks::MockIAccountSynchronizer>>();
+        m_mockAccountSynchronizer =
+            std::make_shared<StrictMock<mocks::MockIAccountSynchronizer>>();
 
     const std::shared_ptr<mocks::MockIAccountSynchronizerFactory>
         m_mockAccountSynchronizerFactory = std::make_shared<
@@ -64,8 +76,8 @@ protected:
             StrictMock<mocks::MockIAuthenticationInfoProvider>>();
 
     const std::shared_ptr<mocks::MockISyncConflictResolver>
-        m_mockSyncConflictResolver = std::make_shared<
-            StrictMock<mocks::MockISyncConflictResolver>>();
+        m_mockSyncConflictResolver =
+            std::make_shared<StrictMock<mocks::MockISyncConflictResolver>>();
 
     const std::shared_ptr<local_storage::tests::mocks::MockILocalStorage>
         m_mockLocalStorage = std::make_shared<
@@ -102,8 +114,7 @@ TEST_F(SynchronizerTest, CtorNullAuthenticationInfoProvider)
 TEST_F(SynchronizerTest, AuthenticateNewAccount)
 {
     const auto synchronizer = std::make_shared<Synchronizer>(
-        m_mockAccountSynchronizerFactory,
-        m_mockAuthenticationInfoProvider);
+        m_mockAccountSynchronizerFactory, m_mockAuthenticationInfoProvider);
 
     const auto authenticationInfo = std::make_shared<AuthenticationInfo>();
 
@@ -120,8 +131,7 @@ TEST_F(SynchronizerTest, AuthenticateNewAccount)
 TEST_F(SynchronizerTest, AuthenticateAccount)
 {
     const auto synchronizer = std::make_shared<Synchronizer>(
-        m_mockAccountSynchronizerFactory,
-        m_mockAuthenticationInfoProvider);
+        m_mockAccountSynchronizerFactory, m_mockAuthenticationInfoProvider);
 
     const auto authenticationInfo = std::make_shared<AuthenticationInfo>();
 
@@ -141,8 +151,7 @@ TEST_F(SynchronizerTest, AuthenticateAccount)
 TEST_F(SynchronizerTest, RevokeAuthentication)
 {
     const auto synchronizer = std::make_shared<Synchronizer>(
-        m_mockAccountSynchronizerFactory,
-        m_mockAuthenticationInfoProvider);
+        m_mockAccountSynchronizerFactory, m_mockAuthenticationInfoProvider);
 
     const qevercloud::UserID userId = 42;
 
@@ -152,6 +161,213 @@ TEST_F(SynchronizerTest, RevokeAuthentication)
             IAuthenticationInfoProvider::ClearCacheOption::User{userId}}));
 
     synchronizer->revokeAuthentication(userId);
+}
+
+TEST_F(SynchronizerTest, SynchronizeAccount)
+{
+    const auto synchronizer = std::make_shared<Synchronizer>(
+        m_mockAccountSynchronizerFactory, m_mockAuthenticationInfoProvider);
+
+    bool onSyncChunksDownloadProgressCalled = false;
+    bool onSyncChunksDownloadedCalled = false;
+    bool onSyncChunksDataProcessingProgressCalled = false;
+    bool onStartLinkedNotebooksDataDownloadingCalled = false;
+    bool onLinkedNotebookSyncChunksDownloadProgressCalled = false;
+    bool onLinkedNotebookSyncChunksDownloadedCalled = false;
+    bool onLinkedNotebookSyncChunksDataProcessingProgressCalled = false;
+    bool onNotesDownloadProgressCalled = false;
+    bool onLinkedNotebookNotesDownloadProgressCalled = false;
+    bool onResourcesDownloadProgressCalled = false;
+    bool onLinkedNotebookResourcesDownloadProgressCalled = false;
+    bool onUserOwnSendStatusUpdateCalled = false;
+    bool onLinkedNotebookSendStatusUpdateCalled = false;
+
+    auto promise = std::make_shared<QPromise<ISyncResultPtr>>();
+    promise->start();
+    auto future = promise->future();
+
+    const auto syncOptions = SyncOptionsBuilder{}.build();
+
+    EXPECT_CALL(
+        *m_mockAccountSynchronizerFactory,
+        createAccountSynchronizer(m_account, _, _, syncOptions))
+        .WillOnce(Return(m_mockAccountSynchronizer));
+
+    std::shared_ptr<IAccountSynchronizer::ICallback> callback;
+
+    EXPECT_CALL(*m_mockAccountSynchronizer, synchronize)
+        .WillOnce(
+            [&](const IAccountSynchronizer::ICallbackWeakPtr & callbackWeak,
+                [[maybe_unused]] const utility::cancelers::ICancelerPtr &
+                    canceler) {
+                callback = callbackWeak.lock();
+                return future;
+            });
+
+    auto result = synchronizer->synchronizeAccount(
+        m_account, m_mockSyncConflictResolver, m_mockLocalStorage, syncOptions,
+        m_canceler);
+
+    ASSERT_TRUE(callback);
+
+    const auto * notifier = result.second;
+
+    QObject::connect(
+        notifier, &ISyncEventsNotifier::syncChunksDownloadProgress, notifier,
+        [&]([[maybe_unused]] const qint32 highestDownloadedUsn,
+            [[maybe_unused]] const qint32 highestServerUsn,
+            [[maybe_unused]] const qint32 lastPreviousUsn) {
+            onSyncChunksDownloadProgressCalled = true;
+        });
+
+    QObject::connect(
+        notifier, &ISyncEventsNotifier::syncChunksDownloaded, notifier,
+        [&] { onSyncChunksDownloadedCalled = true; });
+
+    QObject::connect(
+        notifier, &ISyncEventsNotifier::syncChunksDataProcessingProgress,
+        notifier,
+        [&]([[maybe_unused]] const ISyncChunksDataCountersPtr & counters) {
+            onSyncChunksDataProcessingProgressCalled = true;
+        });
+
+    QObject::connect(
+        notifier, &ISyncEventsNotifier::startLinkedNotebooksDataDownloading,
+        notifier,
+        [&]([[maybe_unused]] const QList<qevercloud::LinkedNotebook> &
+                linkedNotebooks) {
+            onStartLinkedNotebooksDataDownloadingCalled = true;
+        });
+
+    QObject::connect(
+        notifier,
+        &ISyncEventsNotifier::linkedNotebookSyncChunksDownloadProgress,
+        notifier,
+        [&]([[maybe_unused]] qint32 highestDownloadedUsn,
+            [[maybe_unused]] qint32 highestServerUsn,
+            [[maybe_unused]] qint32 lastPreviousUsn,
+            [[maybe_unused]] const qevercloud::LinkedNotebook &
+                linkedNotebook) {
+            onLinkedNotebookSyncChunksDownloadProgressCalled = true;
+        });
+
+    QObject::connect(
+        notifier, &ISyncEventsNotifier::linkedNotebookSyncChunksDownloaded,
+        notifier,
+        [&]([[maybe_unused]] const qevercloud::LinkedNotebook &
+                linkedNotebook) {
+            onLinkedNotebookSyncChunksDownloadedCalled = true;
+        });
+
+    QObject::connect(
+        notifier,
+        &ISyncEventsNotifier::linkedNotebookSyncChunksDataProcessingProgress,
+        notifier,
+        [&]([[maybe_unused]] const ISyncChunksDataCountersPtr & counters,
+            [[maybe_unused]] const qevercloud::LinkedNotebook &
+                linkedNotebook) {
+            onLinkedNotebookSyncChunksDataProcessingProgressCalled = true;
+        });
+
+    QObject::connect(
+        notifier, &ISyncEventsNotifier::notesDownloadProgress, notifier,
+        [&]([[maybe_unused]] const quint32 notesDownloaded,
+            [[maybe_unused]] const quint32 totalNotesToDownload) {
+            onNotesDownloadProgressCalled = true;
+        });
+
+    QObject::connect(
+        notifier, &ISyncEventsNotifier::linkedNotebookNotesDownloadProgress,
+        notifier,
+        [&]([[maybe_unused]] const quint32 notesDownloaded,
+            [[maybe_unused]] const quint32 totalNotesToDownload,
+            [[maybe_unused]] const qevercloud::LinkedNotebook &
+                linkedNotebook) {
+            onLinkedNotebookNotesDownloadProgressCalled = true;
+        });
+
+    QObject::connect(
+        notifier, &ISyncEventsNotifier::resourcesDownloadProgress, notifier,
+        [&]([[maybe_unused]] const quint32 resourcesDownloaded,
+            [[maybe_unused]] const quint32 totalResourcesToDownload) {
+            onResourcesDownloadProgressCalled = true;
+        });
+
+    QObject::connect(
+        notifier, &ISyncEventsNotifier::linkedNotebookResourcesDownloadProgress,
+        notifier,
+        [&]([[maybe_unused]] const quint32 resourcesDownloaded,
+            [[maybe_unused]] const quint32 totalResourcesToDownload,
+            [[maybe_unused]] const qevercloud::LinkedNotebook &
+                linkedNotebook) {
+            onLinkedNotebookResourcesDownloadProgressCalled = true;
+        });
+
+    QObject::connect(
+        notifier, &ISyncEventsNotifier::userOwnSendStatusUpdate, notifier,
+        [&]([[maybe_unused]] const ISendStatusPtr & sendStatus) {
+            onUserOwnSendStatusUpdateCalled = true;
+        });
+
+    QObject::connect(
+        notifier, &ISyncEventsNotifier::linkedNotebookSendStatusUpdate,
+        notifier,
+        [&]([[maybe_unused]] const qevercloud::Guid & linkedNotebookGuid,
+            [[maybe_unused]] const ISendStatusPtr & sendStatus) {
+            onLinkedNotebookSendStatusUpdateCalled = true;
+        });
+
+    callback->onSyncChunksDownloadProgress(42, 42, 42);
+    callback->onSyncChunksDownloaded();
+    callback->onSyncChunksDataProcessingProgress(nullptr);
+    callback->onStartLinkedNotebooksDataDownloading(
+        QList<qevercloud::LinkedNotebook>{});
+
+    callback->onLinkedNotebookSyncChunksDownloadProgress(
+        42, 42, 42, qevercloud::LinkedNotebook{});
+
+    callback->onLinkedNotebookSyncChunksDownloaded(
+        qevercloud::LinkedNotebook{});
+
+    callback->onLinkedNotebookSyncChunksDataProcessingProgress(
+        nullptr, qevercloud::LinkedNotebook{});
+
+    callback->onNotesDownloadProgress(42, 42);
+    callback->onLinkedNotebookNotesDownloadProgress(
+        42, 42, qevercloud::LinkedNotebook{});
+
+    callback->onResourcesDownloadProgress(42, 42);
+    callback->onLinkedNotebookResourcesDownloadProgress(
+        42, 42, qevercloud::LinkedNotebook{});
+
+    callback->onUserOwnSendStatusUpdate(nullptr);
+    callback->onLinkedNotebookSendStatusUpdate(qevercloud::Guid{}, nullptr);
+
+    QCoreApplication::processEvents();
+
+    EXPECT_TRUE(onSyncChunksDownloadProgressCalled);
+    EXPECT_TRUE(onSyncChunksDownloadedCalled);
+    EXPECT_TRUE(onSyncChunksDataProcessingProgressCalled);
+    EXPECT_TRUE(onStartLinkedNotebooksDataDownloadingCalled);
+    EXPECT_TRUE(onLinkedNotebookSyncChunksDownloadProgressCalled);
+    EXPECT_TRUE(onLinkedNotebookSyncChunksDownloadedCalled);
+    EXPECT_TRUE(onLinkedNotebookSyncChunksDataProcessingProgressCalled);
+    EXPECT_TRUE(onNotesDownloadProgressCalled);
+    EXPECT_TRUE(onLinkedNotebookNotesDownloadProgressCalled);
+    EXPECT_TRUE(onResourcesDownloadProgressCalled);
+    EXPECT_TRUE(onLinkedNotebookResourcesDownloadProgressCalled);
+    EXPECT_TRUE(onUserOwnSendStatusUpdateCalled);
+    EXPECT_TRUE(onLinkedNotebookSendStatusUpdateCalled);
+
+    auto syncResult = std::make_shared<SyncResult>();
+    promise->addResult(syncResult);
+    promise->finish();
+
+    QCoreApplication::processEvents();
+
+    ASSERT_TRUE(result.first.isFinished());
+    ASSERT_EQ(result.first.resultCount(), 1);
+    EXPECT_EQ(result.first.result(), syncResult);
 }
 
 } // namespace quentier::synchronization::tests
