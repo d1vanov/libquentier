fe0b8e0c89528ce86022a3a82868a51e5559f5e4
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2024-01-02 19:57:10 +0300

    Some further steps towards the possibility to build with Qt6

---
+++ cmake/modules/LibquentierFindDependencies.cmake
@@ -0,0 +1,60 @@
+set(_LIBQUENTIER_QT_COMPONENTS
+  Core
+  Gui
+  LinguistTools
+  Network
+  PrintSupport
+  Sql
+  Test
+  Xml
+  Widgets)
+if(BUILD_WITH_NOTE_EDITOR)
+  list(APPEND _LIBQUENTIER_QT_COMPONENTS
+    WebChannel
+    WebEngineCore
+    WebEngineWidgets
+    WebSockets
+  )
+endif()
+
+if(NOT ${CMAKE_SYSTEM_NAME} STREQUAL "Windows" AND NOT ${CMAKE_SYSTEM_NAME} STREQUAL "Darwin")
+  list(APPEND _LIBQUENTIER_QT_COMPONENTS
+    DBus
+  )
+endif()
+
+if(_LIBQUENTIER_USE_QT6)
+  if(LIBQUENTIER_HAS_NOTE_EDITOR)
+    set(MIN_QT_VERSION 6.4.0)
+  else()
+    set(MIN_QT_VERSION 6.0.0)
+  endif()
+  find_package(Qt6 6.4.0 COMPONENTS ${_LIBQUENTIER_QT_COMPONENTS} ${LIBQUENTIER_FIND_DEPS_ARGS})
+  message(STATUS "Found Qt6 installation, version ${Qt6Core_VERSION}")
+
+  find_package(QEverCloud-qt6 ${LIBQUENTIER_FIND_DEPS_ARGS})
+  find_package(Qt6Keychain ${LIBQUENTIER_FIND_DEPS_ARGS})
+else()
+  find_package(Qt5 5.12.0 COMPONENTS ${_LIBQUENTIER_QT_COMPONENTS} ${LIBQUENTIER_FIND_DEPS_ARGS})
+  message(STATUS "Found Qt5 installation, version ${Qt5Core_VERSION}")
+
+  find_package(QEverCloud-qt5 ${LIBQUENTIER_FIND_DEPS_ARGS})
+  find_package(Qt5Keychain ${LIBQUENTIER_FIND_DEPS_ARGS})
+endif()
+
+find_package(LibXml2 ${LIBQUENTIER_FIND_DEPS_ARGS})
+add_library(Libxml2::Libxml2 SHARED IMPORTED)
+if(MSVC)
+  set_target_properties(Libxml2::Libxml2 PROPERTIES
+    IMPORTED_IMPLIB ${LIBXML2_LIBRARIES}
+  )
+else()
+  set_target_properties(Libxml2::Libxml2 PROPERTIES
+    IMPORTED_LOCATION ${LIBXML2_LIBRARIES}
+  )
+endif()
+
+find_package(OpenSSL ${LIBQUENTIER_FIND_DEPS_ARGS})
+
+include("${CMAKE_CURRENT_LIST_DIR}/LibquentierFindTidyHtml5.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/LibquentierFindHunspell.cmake")
--- cmake/modules/LibquentierFindQEverCloud.cmake
+++
@@ -1,8 +0,0 @@
-find_package(QEverCloud-qt5 QUIET REQUIRED)
-
-include_directories(${QEVERCLOUD_INCLUDE_DIRS})
-
-get_target_property(QEVERCLOUD_LIBRARY_LOCATION QEverCloud::qt5qevercloud LOCATION)
-message(STATUS "Found QEverCloud library: ${QEVERCLOUD_LIBRARY_LOCATION}")
-
-get_filename_component(QEVERCLOUD_LIB_DIR "${QEVERCLOUD_LIBRARY_LOCATION}" PATH)
