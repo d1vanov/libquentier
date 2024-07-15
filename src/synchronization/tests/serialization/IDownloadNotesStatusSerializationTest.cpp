ffabac3603e14c9b8495cdbcc5027486398f80db
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2024-07-15 21:32:10 +0300

    Unit test for IDownloadNotesStatus serialization and deserialization

---
+++ src/synchronization/tests/serialization/IDownloadNotesStatusSerializationTest.cpp
@@ -0,0 +1,115 @@
+#include <synchronization/types/DownloadNotesStatus.h>
+
+#include <quentier/exception/RuntimeError.h>
+#include <quentier/utility/UidGenerator.h>
+
+#include <qevercloud/types/builders/NoteBuilder.h>
+
+#include <gtest/gtest.h>
+
+#include <array>
+
+// clazy:excludeall=non-pod-global-static
+// clazy:excludeall=returning-void-expression
+
+namespace quentier::synchronization::tests {
+
+class IDownloadNotesStatusSerializationTest :
+    public testing::TestWithParam<StopSynchronizationError>
+{};
+
+constexpr std::array gStopSynchronizationErrors{
+    StopSynchronizationError{std::monostate{}},
+    StopSynchronizationError{RateLimitReachedError{}},
+    StopSynchronizationError{RateLimitReachedError{42}},
+    StopSynchronizationError{AuthenticationExpiredError{}},
+};
+
+INSTANTIATE_TEST_SUITE_P(
+    IDownloadNotesStatusSerializationTestInstance,
+    IDownloadNotesStatusSerializationTest,
+    testing::ValuesIn(gStopSynchronizationErrors));
+
+TEST_P(IDownloadNotesStatusSerializationTest, SerializeAndDeserialize)
+{
+    qint32 updateSequenceNumber = 300;
+    qint32 noteCounter = 1;
+
+    const auto generateNote = [&] {
+        return qevercloud::NoteBuilder{}
+            .setLocalId(UidGenerator::Generate())
+            .setGuid(UidGenerator::Generate())
+            .setTitle(QString::fromUtf8("Note #%1").arg(noteCounter++))
+            .setUpdateSequenceNum(updateSequenceNumber++)
+            .build();
+    };
+
+    auto status = std::make_shared<DownloadNotesStatus>();
+    status->m_totalNewNotes = 42;
+    status->m_totalUpdatedNotes = 43;
+    status->m_totalExpungedNotes = 44;
+
+    constexpr int itemCount = 1;
+
+    status->m_notesWhichFailedToDownload.reserve(itemCount);
+    for (int i = 0; i < itemCount; ++i) {
+        status->m_notesWhichFailedToDownload
+            << IDownloadNotesStatus::NoteWithException{
+                   generateNote(),
+                   std::make_shared<RuntimeError>(
+                       ErrorString{QStringLiteral("Failed to download note")})};
+    }
+
+    status->m_notesWhichFailedToProcess.reserve(itemCount);
+    for (int i = 0; i < itemCount; ++i) {
+        status->m_notesWhichFailedToProcess
+            << IDownloadNotesStatus::NoteWithException{
+                   generateNote(),
+                   std::make_shared<RuntimeError>(
+                       ErrorString{QStringLiteral("Failed to process note")})};
+    }
+
+    status->m_noteGuidsWhichFailedToExpunge.reserve(itemCount);
+    for (int i = 0; i < itemCount; ++i) {
+        status->m_noteGuidsWhichFailedToExpunge
+            << IDownloadNotesStatus::GuidWithException{
+                   UidGenerator::Generate(),
+                   std::make_shared<RuntimeError>(
+                       ErrorString{QStringLiteral("Failed to expunge note")})};
+    }
+
+    status->m_processedNoteGuidsAndUsns.reserve(itemCount);
+    for (int i = 0; i < itemCount; ++i) {
+        status->m_processedNoteGuidsAndUsns[UidGenerator::Generate()] =
+            updateSequenceNumber++;
+    }
+
+    status->m_cancelledNoteGuidsAndUsns.reserve(itemCount);
+    for (int i = 0; i < itemCount; ++i) {
+        status->m_cancelledNoteGuidsAndUsns[UidGenerator::Generate()] =
+            updateSequenceNumber++;
+    }
+
+    status->m_expungedNoteGuids.reserve(itemCount);
+    for (int i = 0; i < itemCount; ++i) {
+        status->m_expungedNoteGuids << UidGenerator::Generate();
+    }
+
+    status->m_stopSynchronizationError = GetParam();
+
+    const auto serialized = status->serializeToJson();
+
+    const auto deserialized =
+        IDownloadNotesStatus::deserializeFromJson(serialized);
+    ASSERT_TRUE(deserialized);
+
+    const auto concreteDeserializedDownloadNotesStatus =
+        std::dynamic_pointer_cast<DownloadNotesStatus>(deserialized);
+    ASSERT_TRUE(concreteDeserializedDownloadNotesStatus);
+
+    EXPECT_EQ(*concreteDeserializedDownloadNotesStatus, *status)
+        << "Lhs: " << concreteDeserializedDownloadNotesStatus->toString().toStdString()
+        << "\nRhs: " << status->toString().toStdString();
+}
+
+} // namespace quentier::synchronization::tests
